35162885ce4e6dba5056468685fe3a2d477675c8
CM Lubinski <christopher.lubinski@gsa.gov>
2017-01-24 16:15:16 +0000

    Add database setup using dj_database_url
    Defaults to the current dir + 'db.sqlite3'

--- omb_eregs/settings.py
+++ omb_eregs/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,10 +77,8 @@ WSGI_APPLICATION = 'omb_eregs.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
 }
 
 
