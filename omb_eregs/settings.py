d4a569f15ccba4094dbee630c166a3e847182d23
CM Lubinski <christopher.lubinski@gsa.gov>
2017-01-24 20:18:33 +0000

    Add flake8 + plugins + pytest.

--- omb_eregs/settings.py
+++ omb_eregs/settings.py
@@ -86,18 +86,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.{0}'.format(validator)}
+    for validator in (
+        'UserAttributeSimilarityValidator', 'MinimumLengthValidator',
+        'CommonPasswordValidator', 'NumericPasswordValidator')
 ]
 
 
