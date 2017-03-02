ae871d3ef9bc78e94929ecf1c4ffb63328b7d9c7
CM Lubinski <christopher.lubinski@gsa.gov>
2017-03-01 23:14:49 -0600

    Only restart node when the server.js changes.
    Previously, we were listening to _any_ ui-dist change, including CSS and
    browser changes.

--- devops/dev_ui.sh
+++ devops/dev_ui.sh
@@ -5,4 +5,4 @@
 # On file change, rebuild JS
 ./node_modules/.bin/webpack --watch &
 # On server.js change, restart node server
-./node_modules/.bin/nodemon --watch ui-dist ui-dist/server.js
+./node_modules/.bin/nodemon --watch ui-dist/server.js ui-dist/server.js
