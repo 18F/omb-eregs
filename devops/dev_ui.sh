29981ee257ba1e84cf12add8469fedfcafc82710
CM Lubinski <christopher.lubinski@gsa.gov>
2017-03-01 23:15:39 -0600

    Run npm install in `docker-compose up dev`.

--- devops/dev_ui.sh
+++ devops/dev_ui.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install dependencies if they're not present
+if [ ! -d node_modules ]; then
+  npm install
+fi
 # Ensure we have _a_ build initially
 ./node_modules/.bin/webpack
 # On file change, rebuild JS
