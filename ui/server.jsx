83e1e74dced8e1beecfeba3c1897b5ed80b2a33a
CM Lubinski <cm.lubinski@gmail.com>
2017-03-09 15:12:24 +0000

    Pull react-router rendering into a separate file.
    This standardizes server.jsx a bit, and will be useful when adding additional
    backend endpoints.

--- ui/server.jsx
+++ ui/server.jsx
@@ -4,48 +4,29 @@ import path from 'path';
 import cfenv from 'cfenv';
 import express from 'express';
 import morgan from 'morgan';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { Resolver } from 'react-resolver';
-import { match, RouterContext } from 'react-router';
 
-import addBasicAuth from './basic-auth';
-import routes from './routes';
+import basicAuth from './basic-auth';
 import { setApiUrl } from './globals';
+import routerEndpoint from './endpoints/router';
 
 const app = express();
 const env = cfenv.getAppEnv();
-const credentials = env.getServiceCreds('config');
+const auth = basicAuth(env.getServiceCreds('config'));
 
+/* Global State */
 setApiUrl(process.env.INTERNAL_API);
+
+/* Middleware */
 app.use(morgan('combined'));
 app.use('/static', express.static(path.join('ui-dist', 'static')));
-addBasicAuth(credentials, app);
+if (auth) {
+  app.use(auth);
+}
 
-app.get('*', (req, res) => {
-  match({ routes, location: req.url }, (error, redirectCtx, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else if (redirectCtx) {
-      res.redirect(302, redirectCtx.pathname + redirectCtx.search);
-    } else if (renderProps) {
-      Resolver.resolve(() => <RouterContext {...renderProps} />).then(({ Resolved, data }) => {
-        res.status(200).send(
-          `<html>
-            <head><link rel="stylesheet" href="/static/styles.css" /></head>
-            <body>
-              <div id="app">${renderToString(<Resolved />)}</div>
-              <script>window.__REACT_RESOLVER_PAYLOAD__ = ${JSON.stringify(data)}</script>
-              <script>window.API_URL = "${process.env.PUBLIC_API}";</script>
-              <script src="/static/browser.js"></script>
-            </body>
-          </html>`);
-      });
-    } else {
-      res.status(404).send('Not found');
-    }
-  });
-});
+/* Endpoints */
+app.get('*', routerEndpoint);
+
+/* Start */
 app.listen(env.port, () => {
   console.log(`Listening on ${env.port}`);
 });
