6a92d8c7dfdeb355934f343a26823d6e7a671c4d
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2017-03-13 11:15:14 -0700

    Remove “Software” category from keyword normalization tests to make it clearer what's going on.

--- reqs/tests/import_reqs_tests.py
+++ reqs/tests/import_reqs_tests.py
@@ -19,6 +19,7 @@ SAMPLE_CSV2 = [
     """policyNumber,policyTitle,uriPolicyID,ombPolicyID,policyType,policyIssuanceYear,policySunset,reqId,issuingBody,policySection,policySubSection,reqText,verb,Impacted Entity,reqDeadline,citation,Acquisition/Contracts,Human Capital,Cloud,Data Centers (Keywords),Cybersecurity (Keywords),Privacy (Keywords),Shared Services (Keywords),IT Project Management (Keywords),Software (Keywords),Digital Services (Keywords),Mobile (Keywords),Hardware/Government Furnished Equipment (GFE) (Keywords),"IT Transparency (Open Data, FOIA, Public Records, etc.) (Keywords)",Agency Statistics (Keywords),Customer Services (Keywords),Governance (Keywords),Financial Systems,Budget (Keywords),Governance - Org Structure,Governance - Implementation (Keywords),Data Management/Standards (Keywords),Definitions (Keywords),Reporting (Keywords),Other (Keywords)""",  # noqa
     """1,25 Point Implementation Plan To Reform Federal Information Technology Management,https://www.whitehouse.gov/sites/default/files/omb/assets/egov_docs/25-point-implementation-plan-to-reform-federal-it.pdf,NA,Strategy,12/9/2010,NA,1.15,"Office of the Federal Chief Information Officer, OMB",PART II: EFFECTIVELY MANAGING LARGE-SCALE IT PROGRAMS,NA,"Moving forward, Federal IT programs must be structured to deploy working business functionality in release cycles no longer than 12 months, and, ideally, less than six months, with initial deployment to end users no later than 18 months after the program begins.",Must,All CFO-Act Agencies,NA,NA,,,,,,,,,x,,,,,,,,,,,,,,,Software Development Lifecycle/Agile""",  # noqa
     """21,Data Center Optimization Initiative (DCOI),https://www.whitehouse.gov/sites/default/files/omb/memoranda/2016/m_16_19_1.pdf,M-16-19,M-Memorandum,8/1/2016,9/30/2018,21.44,"Office of the Federal Chief Information Officer, OMB",Metric Target Values,Goal 2: Cost Savings and Avoidance,"Within 30 days after publication of this document, OMB OFCIO will set individual cost savings and cost avoidance goals for each agency.",Will,OMB OFCIO,Within 30 days,NA,,,,,,,,,,,,,X,,,,X,,X,,,,,""",  # noqa
+    """1,25 Point Implementation Plan To Reform Federal Information Technology Management,https://www.whitehouse.gov/sites/default/files/omb/assets/egov_docs/25-point-implementation-plan-to-reform-federal-it.pdf,NA,Strategy,12/9/2010,NA,1.15,"Office of the Federal Chief Information Officer, OMB",PART II: EFFECTIVELY MANAGING LARGE-SCALE IT PROGRAMS,NA,"Moving forward, Federal IT programs must be structured to deploy working business functionality in release cycles no longer than 12 months, and, ideally, less than six months, with initial deployment to end users no later than 18 months after the program begins.",Must,All CFO-Act Agencies,NA,NA,,,,,,,,,,,,,,,,,,,,,,,,Software Development Lifecycle/Agile""",  # noqa
 ]
 
 
@@ -285,7 +286,7 @@ def test_keyword_normalization(blank_csv_file, keyword, expected):
     """
     csv_str = "\n".join([
         SAMPLE_CSV2[0],
-        SAMPLE_CSV2[1].replace("Software Development Lifecycle/Agile", keyword)
+        SAMPLE_CSV2[3].replace("Software Development Lifecycle/Agile", keyword)
     ])
     blank_csv_file.write(csv_str)
 
@@ -293,4 +294,4 @@ def test_keyword_normalization(blank_csv_file, keyword, expected):
 
     reqs = list(Requirement.objects.order_by("req_id"))
     assert len(reqs) == 1
-    assert set(reqs[0].keywords.names()) == set(expected + ["Software"])
+    assert set(reqs[0].keywords.names()) == set(expected)
