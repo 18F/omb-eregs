5314cdba37239a00d55b8bc5087c98b3a4458e61
CM Lubinski <christopher.lubinski@gsa.gov>
2017-03-14 14:57:33 -0400

    Fix linting error.

--- ui/routes.jsx
+++ ui/routes.jsx
@@ -6,14 +6,14 @@ import Keywords from './components/keywords';
 import Index from './components/index';
 import Policies from './components/policies';
 import Requirements from './components/requirements';
-import LookupSearch, { redirectIfMatched } from './components/lookup-search';
+import AsyncLookupSearch, { redirectIfMatched } from './components/lookup-search';
 
 export default <Router history={browserHistory} >
   <Route path="/" component={App}>
     <IndexRoute component={Index} />
     <Route path="keywords">
       <IndexRoute component={Keywords} />
-      <Route path="search-redirect" component={LookupSearch} onEnter={redirectIfMatched} />
+      <Route path="search-redirect" component={AsyncLookupSearch} onEnter={redirectIfMatched} />
     </Route>
     <Route path="policies" component={Policies} />
     <Route path="requirements" component={Requirements} />
