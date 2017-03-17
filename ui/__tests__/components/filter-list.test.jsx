29201eca6b321e57c83694e0cda166742bc7ca67
CM Lubinski <christopher.lubinski@gsa.gov>
2017-03-17 11:17:43 -0400

    Update tests to match FilterList tweaks.

--- ui/__tests__/components/filter-list.test.jsx
+++ ui/__tests__/components/filter-list.test.jsx
@@ -2,16 +2,16 @@ import axios from 'axios';
 import { shallow } from 'enzyme';
 import React from 'react';
 
-import FilterList, { fetchData, Filter } from '../../components/filter-list';
+import FilterList, { fetchKeywords, fetchPolicies, Filter } from '../../components/filter-list';
 
 jest.mock('axios');
 jest.mock('../../globals', () => ({ apiUrl: jest.fn(() => 'api-start/') }));
 
 
-describe('fetchData()', () => {
+describe('fetchKeywords()', () => {
   it('handles an empty query', () => {
     const props = { location: { query: {} } };
-    return fetchData(props).then(result =>
+    return fetchKeywords(props).then(result =>
       expect(result).toHaveLength(0));
   });
   it('hits our API when a keyword is present', () => {
@@ -19,7 +19,7 @@ describe('fetchData()', () => {
     axios.get = jest.fn(
       () => Promise.resolve({ data: { results: [1, 2, 3] } }),
     );
-    return fetchData(props).then((result) => {
+    return fetchKeywords(props).then((result) => {
       expect(result).toEqual([1, 2, 3]);
       expect(axios.get).toHaveBeenCalledWith(
         'api-start/keywords/', { params: { id__in: 'ids,here' } },
@@ -28,35 +28,59 @@ describe('fetchData()', () => {
   });
 });
 
+describe('fetchPolicies()', () => {
+  it('handles an empty query', () => {
+    const props = { location: { query: {} } };
+    return fetchPolicies(props).then(result =>
+      expect(result).toHaveLength(0));
+  });
+  it('hits our API when a keyword is present', () => {
+    const props = { location: { query: { policy_id__in: 'ids,here' } } };
+    axios.get = jest.fn(
+      () => Promise.resolve({ data: { results: [1, 2, 3] } }),
+    );
+    return fetchPolicies(props).then((result) => {
+      expect(result).toEqual([1, 2, 3]);
+      expect(axios.get).toHaveBeenCalledWith(
+        'api-start/policies/', { params: { id__in: 'ids,here' } },
+      );
+    });
+  });
+});
+
 describe('<FilterList />', () => {
   it('passed transformed args to its Filters', () => {
     const params = {
-      router: { location: { query: {
-        keywords__id__in: '1,7,10', some: 'field', page: '5',
-      } } },
-      keywords: [{ id: 1, name: 'a' }],
+      router: { location: { query: { some: 'field', page: '5' } } },
+      lookup: 'keywords',
+      existingFilters: [
+        { id: 1, name: 'a' }, { id: 7, name: 'b' }, { id: 10, name: 'c' },
+      ],
     };
     const filter = shallow(<FilterList {...params} />).find('Filter').first();
-    expect(filter.prop('keywordIds')).toEqual(['1', '7', '10']);
-    expect(filter.prop('keyword')).toEqual({ id: 1, name: 'a' });
-    expect(filter.prop('query')).toEqual({ some: 'field' });
+    expect(filter.prop('existingIds')).toEqual([1, 7, 10]);
+    expect(filter.prop('idToRemove')).toEqual(1);
+    expect(filter.prop('name')).toEqual('a');
+    expect(filter.prop('location')).toEqual(params.router.location);
   });
   it('contains the correct number of Filters', () => {
     const params = {
       router: { location: { query: {} } },
-      keywords: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+      lookup: 'keywords',
+      existingFilters: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
     };
     let result = shallow(<FilterList {...params} />);
     expect(result.find('Filter')).toHaveLength(4);
 
-    params.keywords.pop();
+    params.existingFilters.pop();
     result = shallow(<FilterList {...params} />);
     expect(result.find('Filter')).toHaveLength(3);
   });
   it('contains an AddKeyword', () => {
     const params = {
       router: { location: { query: {} } },
-      keywords: [],
+      lookup: 'keywords',
+      existingFilters: [],
     };
     const result = shallow(<FilterList {...params} />);
     expect(result.find('DoesNotExist')).toHaveLength(0);
@@ -66,9 +90,11 @@ describe('<FilterList />', () => {
 
 describe('<Filter />', () => {
   const params = {
-    keywordIds: ['3', '5', '7'],
-    query: { some: 'stuff' },
-    keyword: { id: 5, name: 'AkeyWord' },
+    existingIds: [3, 5, 7],
+    idToRemove: 5,
+    location: { pathname: '/path/', query: { some: 'stuff' } },
+    name: 'AkeyWord',
+    removeParam: 'someStr',
   };
   const result = shallow(<Filter {...params} />);
 
@@ -78,8 +104,8 @@ describe('<Filter />', () => {
   it('links to the proper destination', () => {
     const link = result.find('Link').first();
     expect(link.prop('to')).toEqual({
-      pathname: '/requirements/',
-      query: { some: 'stuff', keywords__id__in: '3,7' },
+      pathname: '/path/',
+      query: { some: 'stuff', someStr: '3,7' },
     });
   });
 });
