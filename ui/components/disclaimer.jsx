16f13fad3baa99846bf7eca18fe83cdef13a7097
John Donmoyer <jdonmoyer@gmail.com>
2017-03-28 18:24:37 -0500

    initial navbar w/ usa header

---
+++ ui/components/disclaimer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const disclaimer = () => (
+  <div className="overflow-auto">
+    <div className="flex table usa-disclaimer">
+      <img
+        className="pr1 table-cell align-middle"
+        alt="US flag"
+        width="20"
+        height="13"
+        src="/static/img/usa-flag.png"
+      />
+      <div className="table-cell align-middle">
+        An official website of the United States government
+      </div>
+    </div>
+  </div>
+);
+
+export default disclaimer;
---
+++ ui/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const navbar = () => (
+  <div className="overflow-auto">
+    <div className="flex table navbar">
+      <img
+        className="pr1 table-cell align-middle"
+        alt="US flag"
+        width="20"
+        height="13"
+        src="/static/img/usa-flag.png"
+      />
+      <div className="table-cell align-middle navbar-title">
+          OMB Policy
+      </div>
+    </div>
+  </div>
+);
+
+export default navbar;
