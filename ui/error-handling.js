c294f59e364c690af309e2fbdc4a4b34493ca888
CM Lubinski <christopher.lubinski@gsa.gov>
2017-04-05 16:21:30 -0400

    Log exceptions to the console, too.

--- ui/error-handling.js
+++ ui/error-handling.js
@@ -8,6 +8,10 @@ import Error from './components/error';
 /* eslint-disable no-unused-vars */
 export default function (err, req, res, next) {
 /* eslint-enable no-unused-vars */
+  // We're intentionally to log to console
+  /* eslint-disable no-console */
+  console.error(err.stack);
+  /* eslint-enable no-console */
   const error = React.createElement(Error, { err });
   res.status(500).send(renderToStaticMarkup(error));
 }
