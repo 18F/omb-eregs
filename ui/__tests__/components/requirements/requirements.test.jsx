86ccb4d9cf2237435ac7695429e3f92e181d712f
CM Lubinski <cm.lubinski@gmail.com>
2017-04-13 20:37:04 +0000

    Make keywords (within a requirement) a link.
    With test. Should resolve #181

---
+++ ui/__tests__/components/requirements/requirements.test.jsx
@@ -0,0 +1,26 @@
+import { mount } from 'enzyme';
+import React from 'react';
+
+import Requirement from '../../../components/requirements/requirement';
+
+describe('<Requirement />', () => {
+  it('includes links to keywords', () => {
+    const req = {
+      req_id: '1.1',
+      req_text: '',
+      policy: { sunset: null },
+      keywords: [
+        { id: 6, name: 'Six' }, { id: 8, name: 'Eight' },
+      ],
+    };
+
+    const result = mount(<Requirement requirement={req} />);
+    const links = result.find('Link');
+
+    expect(links).toHaveLength(2);
+    const [six, eight] = [links.first(), links.last()];
+    expect(six.text()).toEqual('Six');
+    expect(six.prop('to').query.keywords__id__in).toEqual(6);
+    expect(eight.prop('to').pathname).toEqual('/requirements/by-keyword');
+  });
+});
