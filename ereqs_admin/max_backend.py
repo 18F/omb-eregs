76f8cacb22d1aeff4c20813174c4071d4be5e77b
CM Lubinski <christopher.lubinski@gsa.gov>
2017-04-20 15:48:31 -0400

    Add and test logging around MAX logins.
    This should help debug logins and track malicious attempts.

--- ereqs_admin/max_backend.py
+++ ereqs_admin/max_backend.py
@@ -1,6 +1,19 @@
+import logging
+
 from django.contrib.auth.models import User
 from django_cas_ng.backends import CASBackend
 from django_cas_ng.utils import get_cas_client
+
+logger = logging.getLogger(__name__)
+
+
+def update_user_attributes(user, attributes):
+    # Trim to thirty chars to match Django User max lengths
+    user.first_name = attributes.get('First-Name', '')[:30]
+    user.last_name = attributes.get('Last-Name', '')[:30]
+    user.email = attributes.get('Email-Address', '')
+    user.is_staff = True
+    user.save()
 
 
 class MAXBackend(CASBackend):
@@ -10,17 +23,18 @@ class MAXBackend(CASBackend):
         client = get_cas_client(service_url=service)
         max_username, attributes, _ = client.verify_ticket(ticket)
         if not max_username:    # bad ticket
+            logger.warning("MAX ticket (%s) lookup was unsuccessful. "
+                           "Received attributes: %s", ticket, attributes)
             return None
 
         email = attributes.get('Email-Address', '')
 
         user = User.objects.filter(username=email).first()
         if user:
-            # Trim to thirty chars to match Django User max lengths
-            user.first_name = attributes.get('First-Name', '')[:30]
-            user.last_name = attributes.get('Last-Name', '')[:30]
-            user.email = email
-            user.is_staff = True
-            user.save()
-
+            update_user_attributes(user, attributes)
+            logger.info("User %s successfully logged in", email)
             return user
+        else:
+            logger.warning("MAX ticket (%s) referred to a user not in our "
+                           "system: %s. Received attributes: %s",
+                           ticket, email, attributes)
--- ereqs_admin/tests/max_login_tests.py
+++ ereqs_admin/tests/max_login_tests.py
@@ -7,30 +7,38 @@ from model_mommy import mommy
 from ereqs_admin import max_backend
 
 
-@pytest.fixture
-def max_setup(monkeypatch):
+def mock_verify_ticket(monkeypatch, exists_in_max=True, first='First',
+                       last='Last', email='first.last@example.com'):
+    if exists_in_max:
+        attributes = {
+            'First-Name': first,
+            'Last-Name': last,
+            'Email-Address': email,
+        }
+        max_username = 'max-username'
+    else:
+        attributes = {}
+        max_username = None
+
     monkeypatch.setattr(max_backend, 'get_cas_client', Mock())
-    attributes = {
-        'First-Name': 'Freddie',
-        'Last-Name': 'Mercury',
-        'Email-Address': 'fred@example.com',
-    }
     verify_ticket = max_backend.get_cas_client.return_value.verify_ticket
-    verify_ticket.return_value = (Mock(), attributes, Mock())
+    verify_ticket.return_value = (max_username, attributes, Mock())
+
+    return verify_ticket
 
 
 @pytest.mark.django_db
-@pytest.mark.usefixtures('max_setup')
-def test_authenticate_success():
+def test_authenticate_success(monkeypatch):
     """Verify that a user is configured correctly"""
-    get_cas_client = max_backend.get_cas_client
-    verify_ticket = get_cas_client.return_value.verify_ticket
     user = mommy.make(User, is_staff=False, username='fred@example.com')
+    verify_ticket = mock_verify_ticket(
+        monkeypatch, first='Freddie', last='Mercury', email='fred@example.com')
 
     backend = max_backend.MAXBackend()
     backend.authenticate(Mock(), 'some-ticket', 'service-url')
 
-    assert get_cas_client.call_args == call(service_url='service-url')
+    assert max_backend.get_cas_client.call_args == call(
+        service_url='service-url')
     assert verify_ticket.call_args == call('some-ticket')
 
     user = User.objects.get(pk=user.pk)
@@ -41,10 +49,10 @@ def test_authenticate_success():
 
 
 @pytest.mark.django_db
-@pytest.mark.usefixtures('max_setup')
-def test_authenticate_failure_unknown_user():
+def test_authenticate_failure_unknown_user(monkeypatch):
     """If MAX knows about the user but Django does not, we should fail"""
     assert User.objects.count() == 0
+    mock_verify_ticket(monkeypatch)
 
     backend = max_backend.MAXBackend()
     result = backend.authenticate(Mock(), 'some-ticket', 'service-url')
@@ -56,13 +64,50 @@ def test_authenticate_failure_unknown_user():
 def test_authenticate_ticket_failure(monkeypatch):
     """If MAX isn't aware of the user, we shouldn't explode (nor create a new
     user)"""
-    monkeypatch.setattr(max_backend, 'get_cas_client', Mock())
-    verify_ticket = max_backend.get_cas_client.return_value.verify_ticket
-    verify_ticket.return_value = (None, Mock(), Mock())
-
     assert User.objects.count() == 0
+    mock_verify_ticket(monkeypatch, exists_in_max=False)
 
     backend = max_backend.MAXBackend()
     result = backend.authenticate(Mock(), 'some-ticket', 'service-url')
     assert result is None
     assert User.objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_authenticate_logging_bad_ticket(monkeypatch):
+    monkeypatch.setattr(max_backend, 'logger', Mock())
+    mock_verify_ticket(monkeypatch, exists_in_max=False)
+
+    backend = max_backend.MAXBackend()
+    assert backend.authenticate(Mock(), 'some-ticket', 'service-url') is None
+    assert max_backend.logger.warning.called
+    warning_message = max_backend.logger.warning.call_args[0][0]
+    assert 'lookup was unsuccessful' in warning_message
+
+
+@pytest.mark.django_db
+def test_authenticate_logging_bad_email(monkeypatch):
+    monkeypatch.setattr(max_backend, 'logger', Mock())
+    mock_verify_ticket(monkeypatch, email='bob@example.com')
+    assert User.objects.count() == 0
+
+    backend = max_backend.MAXBackend()
+    assert backend.authenticate(Mock(), 'some-ticket', 'service-url') is None
+    assert max_backend.logger.warning.called
+    warning_message = max_backend.logger.warning.call_args[0][0]
+    assert 'referred to a user not in our system' in warning_message
+    assert 'bob@example.com' in max_backend.logger.warning.call_args[0]
+
+
+@pytest.mark.django_db
+def test_authenticate_logging_success(monkeypatch):
+    mommy.make(User, username='fred@example.com')
+    monkeypatch.setattr(max_backend, 'logger', Mock())
+    mock_verify_ticket(monkeypatch, email='fred@example.com')
+
+    backend = max_backend.MAXBackend()
+    backend.authenticate(Mock(), 'some-ticket', 'service-url')
+
+    assert max_backend.logger.info.called
+    assert 'success' in max_backend.logger.info.call_args[0][0]
+    assert 'fred@example.com' in max_backend.logger.info.call_args[0]
