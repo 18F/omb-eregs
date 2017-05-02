c6305efb7d8bcb5a8dcceb1d35dcb69a63a16981
CM Lubinski <christopher.lubinski@gsa.gov>
2017-05-02 11:31:32 -0400

    Use TabView and PoliciesContainer.
    Continues to separate template/views from container/logic.

--- ui/components/policies/container.js
+++ ui/components/policies/container.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import { resolve } from 'react-resolver';
 
-import api from '../../api';
+import PoliciesView from './policies-view';
 import SearchFilterView from '../search-filter-view';
+import TabView from '../tab-view';
+import api from '../../api';
 
-function PolicyContainer() {
+function requirementsTab() {
+  // This will be dynamic soon
+  const link = { pathname: '/requirements' };
+  return React.createElement(
+    TabView, { active: false, tabName: 'Requirements', link });
+}
+
+function PoliciesContainer({ pagedPolicies }) {
   const filters = [];
-  const tabs = [];
-  const pageContent = null;
+  const tabs = [
+    requirementsTab(),
+    React.createElement(TabView, { active: true, tabName: 'Policies' }),
+  ];
+  const pageContent = React.createElement(
+    PoliciesView,
+    { policies: pagedPolicies.results, count: pagedPolicies.count },
+  );
   return React.createElement(SearchFilterView, { filters, tabs, pageContent });
 }
+PoliciesContainer.propTypes = {
+  pagedPolicies: React.PropTypes.shape({
+    results: PoliciesView.propTypes.policies,
+    count: PoliciesView.propTypes.count,
+  }),
+};
+PoliciesContainer.defaultProps = {
+  pagedPolicies: { results: [], count: 0 },
+};
 
 function fetchPolicies({ location: { query } }) {
   const params = Object.assign({ ordering: 'policy_number' }, query);
@@ -18,5 +42,5 @@ function fetchPolicies({ location: { query } }) {
 
 export default resolve({
   pagedPolicies: fetchPolicies,
-})(PolicyContainer);
+})(PoliciesContainer);
 
