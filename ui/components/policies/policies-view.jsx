318b08f470e3912981624be4e0d7424400afa866
Theresa Summa Lurie <theresa.summa@gsa.gov>
2017-05-03 10:54:58 -0700

    fixes linter errors

--- ui/components/policies/policies-view.jsx
+++ ui/components/policies/policies-view.jsx
@@ -12,25 +12,41 @@ function Policy({ policy, query }) {
     pathname: '/requirements',
     query: {
       policy_id: policy.id,
-      topics__id__in: query['requirements__topics__id__in'],
+      topics__id__in: query.requirements__topics__id__in,
     },
   };
   return (
     <li key={policy.id}>
       <h3>{policy.title}</h3>
-      <Link to={relevantReqs}>{policy.relevant_reqs}</Link> of <Link to={allReqs}>{policy.total_reqs}</Link> match your search
+      <Link to={relevantReqs}>
+        {policy.relevant_reqs}
+      </Link>
+       of
+      <Link to={allReqs}>
+        {policy.total_reqs}
+      </Link>
+       match your search
       <span className="download-policy-link">Download policy PDF</span>
-      <span className="view-requirements-link"><Link to={allReqs}>View all requirements</Link></span>
+      <span className="view-requirements-link">
+        <Link to={allReqs}>View all requirements</Link>
+      </span>
     </li>
   );
 }
 
 Policy.propTypes = {
-   policies: React.PropTypes.arrayOf(React.PropTypes.shape({
+  policy: React.PropTypes.shape({
     id: React.PropTypes.number,
     title: React.PropTypes.string,
     relevant_reqs: React.PropTypes.number,
-  })),
+    total_reqs: React.PropTypes.number,
+  }),
+  query: React.PropTypes.shape({}),
+};
+
+Policy.defaultProps = {
+  policy: [],
+  query: {},
 };
 
 export default function PoliciesView({ policies, count, query }) {
@@ -38,7 +54,7 @@ export default function PoliciesView({ policies, count, query }) {
     <div>
       <ul className="list-reset">
         { policies.map(policy =>
-          <Policy key={policy.id} policy={policy} query={query} />
+          <Policy key={policy.id} policy={policy} query={query} />,
         )}
       </ul>
       <Pagers count={count} />
@@ -48,12 +64,12 @@ export default function PoliciesView({ policies, count, query }) {
 PoliciesView.propTypes = {
   policies: React.PropTypes.arrayOf(React.PropTypes.shape({
     id: React.PropTypes.number,
-    title: React.PropTypes.string,
-    relevant_reqs: React.PropTypes.number,
   })),
   count: React.PropTypes.number,
+  query: React.PropTypes.shape({}),
 };
 PoliciesView.defaultProps = {
   policies: [],
   count: 0,
+  query: {},
 };
