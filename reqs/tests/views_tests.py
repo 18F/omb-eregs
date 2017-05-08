79d14ecfc00562155fb00623498be49be4cab750
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2017-05-08 16:00:41 -0700

    Use better names for lists of policies in test of non-poublic policies, per CM's suggestion.

--- reqs/tests/views_tests.py
+++ reqs/tests/views_tests.py
@@ -215,21 +215,22 @@ def test_policies_counts_filter_by_multiple_topics(policy_setup):
 @pytest.mark.django_db
 def test_excludes_nonpublic():
     """The API endpoint should not include policies flagged as nonpublic"""
-    policies = [mommy.make(Policy, policy_number='0'),
-                mommy.make(Policy, policy_number='1', nonpublic=True)]
-    reqs = [mommy.make(Requirement, policy=policies[0], _quantity=3),
-            mommy.make(Requirement, policy=policies[1], _quantity=4)]
+    public_policy = mommy.make(Policy, policy_number='0')
+    nonpublic_policy = mommy.make(Policy, policy_number='1', nonpublic=True)
+    public_reqs = mommy.make(Requirement, policy=public_policy, _quantity=3)
+    nonpublic_reqs = mommy.make(Requirement,
+                                policy=nonpublic_policy, _quantity=4)
     client = APIClient()
 
     path = "/policies/"
     response = client.get(path).json()
     assert response['count'] == 1
 
-    public_path = "/policies/?id={0}".format(policies[0].id)
+    public_path = "/policies/?id={0}".format(public_policy.id)
     public_response = client.get(public_path).json()
     assert public_response['count'] == 1
 
-    nonpublic_path = "/policies/?id={0}".format(policies[1].id)
+    nonpublic_path = "/policies/?id={0}".format(nonpublic_policy.id)
     nonpublic_response = client.get(nonpublic_path).json()
     assert nonpublic_response['count'] == 0
 
@@ -237,10 +238,12 @@ def test_excludes_nonpublic():
     req_response = client.get(req_path).json()
     assert req_response['count'] == 3
 
-    id_path = "/requirements/?req_id=" + reqs[0][1].req_id
-    id_response = client.get(id_path).json()
-    assert id_response['count'] == 1
+    for req in public_reqs:
+        id_path = "/requirements/?req_id=" + req.req_id
+        id_response = client.get(id_path).json()
+        assert id_response['count'] == 1
 
-    id_nonpublic_path = "/requirements/?req_id=" + reqs[1][1].req_id
-    id_nonpublic_response = client.get(id_nonpublic_path).json()
-    assert id_nonpublic_response['count'] == 0
+    for req in nonpublic_reqs:
+        id_nonpublic_path = "/requirements/?req_id=" + req.req_id
+        id_nonpublic_response = client.get(id_nonpublic_path).json()
+        assert id_nonpublic_response['count'] == 0
