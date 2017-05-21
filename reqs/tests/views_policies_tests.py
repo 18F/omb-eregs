2f06cbec5ef32b74dff2c4c09ae923fe045a1c8f
CM Lubinski <christopher.lubinski@gsa.gov>
2017-05-20 22:26:13 -0700

    Rework policy-related fixture.
    We'll be adding tests around agencies soon, so we don't need the topics to be
    added.

--- reqs/tests/views_policies_tests.py
+++ reqs/tests/views_policies_tests.py
@@ -6,29 +6,35 @@ from rest_framework.test import APIClient
 
 from reqs.models import Policy, Requirement, Topic
 
-PolicySetup = namedtuple('PolicySetup', ('topics', 'policies', 'reqs'))
+PolicySetup = namedtuple('PolicySetup', ('policies', 'reqs'))
 
 
 @pytest.fixture
 def policy_setup():
-    topics = mommy.make(Topic, _quantity=3)
     policies = [mommy.make(Policy, policy_number='0'),
                 mommy.make(Policy, policy_number='1')]
     reqs = [mommy.make(Requirement, policy=policies[0], _quantity=3),
             mommy.make(Requirement, policy=policies[1], _quantity=4)]
+    yield PolicySetup(policies, reqs)
+
+
+@pytest.fixture
+def policy_topic_setup(policy_setup):
+    topics = mommy.make(Topic, _quantity=3)
+    reqs = policy_setup.reqs
     reqs[0][0].topics.add(topics[0].name)
     reqs[0][1].topics.add(topics[1].name)
     reqs[0][2].topics.add(topics[0].name, topics[1].name)
     reqs[1][0].topics.add(topics[1].name)
     reqs[1][1].topics.add(topics[1].name, topics[2].name)
-    yield PolicySetup(topics, policies, reqs)
+    yield policy_setup, topics
 
 
 @pytest.mark.django_db
-def test_policies_counts_no_params(policy_setup):
+def test_topics_counts_no_params(policy_topic_setup):
     """The API endpoint should include all requirements when no params are
     given"""
-    _, _, reqs = policy_setup
+    (_, reqs), _ = policy_topic_setup
     client = APIClient()
 
     response = client.get("/policies/").json()
@@ -40,10 +46,10 @@ def test_policies_counts_no_params(policy_setup):
 
 
 @pytest.mark.django_db
-def test_policies_counts_filter_req(policy_setup):
+def test_topics_counts_filter_req(policy_topic_setup):
     """The API endpoint should include only relevant policies when we filter
     by an attribute of a requirement"""
-    _, _, reqs = policy_setup
+    (_, reqs), _ = policy_topic_setup
     client = APIClient()
 
     path = "/policies/?requirements__req_id=" + reqs[1][1].req_id
@@ -54,10 +60,10 @@ def test_policies_counts_filter_req(policy_setup):
 
 
 @pytest.mark.django_db
-def test_policies_counts_filter_by_one_topic(policy_setup):
+def test_topics_counts_filter_by_one_topic(policy_topic_setup):
     """The API endpoint should include only relevant policies when we filter
     by a single topic"""
-    topics, _, reqs = policy_setup
+    (_, reqs), topics = policy_topic_setup
     client = APIClient()
 
     path = "/policies/?requirements__topics__id__in={0}".format(topics[0].pk)
@@ -69,10 +75,10 @@ def test_policies_counts_filter_by_one_topic(policy_setup):
 
 
 @pytest.mark.django_db
-def test_policies_counts_filter_by_multiple_topics(policy_setup):
+def test_topics_counts_filter_by_multiple_topics(policy_topic_setup):
     """The API endpoint should include only relevant policies when we filter
     by multiple topics"""
-    topics, _, reqs = policy_setup
+    (_, reqs), topics = policy_topic_setup
     client = APIClient()
 
     path = "/policies/?requirements__topics__id__in={0},{1}".format(
