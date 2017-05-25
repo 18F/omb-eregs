0f457de1902082fbf7dea7cc5b2cf3cb7959c763
Theresa Summa Lurie <theresa.summa@gsa.gov>
2017-05-25 15:16:04 -0700

    adds initial homepage and footer styling

--- ui/components/footer.jsx
+++ ui/components/footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
-      <h2>OMB Policy Library</h2>
+    <footer className="global-footer px4 py3">
+      <h4>OMB Policy Library</h4>
     </footer>
   );
 }
--- ui/components/homepage.jsx
+++ ui/components/homepage.jsx
@@ -16,27 +16,33 @@ export default function Homepage() {
   ];
 
   return (
-    <div>
-      <section>
-        <h2>About this site</h2>
-        <p>
-            The OMB Policy Library includes excerpts from memos and
-            policy documents issued by the White House. This project
-            is part of our ongoing efforts to make it easier to find,
-            read, and understand information technology requirements.
-        </p>
-        <p>
-            The information on this site should be considered unofficial
-            and will be updated frequently as a convenience to agencies
-            and the public. For official OMB guidance, please follow the
-            links to the original memos and policy documents.
-        </p>
+    <div className="homepage">
+      <section className="filter-form px4 py2 center">
+        <h2>Find policies and requirements that apply to your agency.</h2>
+        <div className="filter px4">
+          <label>What topics are you interested in?</label>
+          <div className="form-field">
+            { filterControls }
+          </div>
+        </div>
       </section>
 
-      <section>
-        <h3>Find policies and requirements that apply to your agency.</h3>
-        <h4>What topics are you interested in?</h4>
-        { filterControls }
+      <section className="about px4 py3">
+        <div className="about-inner px2 mx4">
+          <h3>About this site</h3>
+          <p>
+              The OMB Policy Library includes excerpts from memos and
+              policy documents issued by the White House. This project
+              is part of our ongoing efforts to make it easier to find,
+              read, and understand information technology requirements.
+          </p>
+          <p>
+              The information on this site should be considered unofficial
+              and will be updated frequently as a convenience to agencies
+              and the public. For official OMB guidance, please follow the
+              links to the original memos and policy documents.
+          </p>
+        </div>
       </section>
     </div>
   );
