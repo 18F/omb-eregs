c0fb77d0bedc76958cc64652ed9ba682540c5081
Theresa Summa Lurie <theresa.summa@gsa.gov>
2017-06-06 16:00:56 -0700

    make search work everywhere

--- ui/components/search/search.jsx
+++ ui/components/search/search.jsx
@@ -11,12 +11,22 @@ export default class Search extends React.Component {
   }
 
   inputName() {
-    return this.context.router.location.pathname.includes('requirements') ? 'req_text__search' : 'requirements__req_text__search';
+    return this.context.router.location.pathname.includes('policies') ? 'requirements__req_text__search' : 'req_text__search';
+  }
+
+  actionPath() {
+    const path = this.context.router.location.pathname;
+    if (path.includes('policies') || path.includes('requirements')) {
+      return path;
+    }
+    else {
+      return 'requirements/';
+    }
   }
 
   render() {
     return (
-      <form method="GET" action={this.context.router.location.pathname}>
+      <form method="GET" action={this.actionPath()}>
         <input name={this.inputName()} type="text" placeholder="Search..." />
         { this.hiddenFields() }
         <input type="submit" value="Submit" />
