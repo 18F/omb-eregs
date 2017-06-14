a89c3629846a516de33d587d631a1a812ec5afb1
CM Lubinski <christopher.lubinski@gsa.gov>
2017-06-14 17:36:25 -0400

    Replace hard-coded values w/ factory.
    This change should allow readers to see the values relevant to tests more
    clearly.

--- reqs/tests/import_reqs_tests.py
+++ reqs/tests/import_reqs_tests.py
@@ -1,8 +1,10 @@
+import random
 from datetime import date
 from unittest.mock import Mock
 
 import pytest
 from django.core.management import call_command
+from django.utils.crypto import get_random_string
 from model_mommy import mommy
 
 from reqs.management.commands import import_reqs
@@ -136,12 +138,38 @@ def test_priority_split(text, result):
     assert import_reqs.priority_split(text, ';', ',') == result
 
 
+def policy_row_factory(**kwargs):
+    """Generate random data in a policy-row format"""
+    row = dict(
+        policyNumber=str(random.randint(100, 999)),                 # nosec
+        policyTitle=get_random_string(32),
+        uriPolicyId='http://example.com/' + get_random_string(10),
+        ombPolicyId=get_random_string(32),
+        policyType=random.choice([t.value for t in PolicyTypes]),   # nosec
+        policyIssuanceYear=date(
+            random.randint(1950, 2016),                             # nosec
+            random.randint(1, 12),                                  # nosec
+            random.randint(1, 28)).strftime('%m/%d/%Y'),            # nosec
+        policySunset='NA',
+        issuingBody=get_random_string(10)
+    )
+    row.update(kwargs)
+    return row
+
+
 @pytest.mark.django_db
 def test_policy_from_row():
-    row = {'policyNumber': '123', 'policyTitle': 'title 1',
-           'uriPolicyId': 'http://example.com/a', 'ombPolicyId': 'policy',
-           'policyType': 'Memo', 'policyIssuanceYear': '12/20/2001',
-           'policySunset': 'NA', 'issuingBody': 'IBIB'}
+    row = policy_row_factory(
+        policyNumber='123',
+        policyTitle='title 1',
+        uriPolicyId='http://example.com/a',
+        ombPolicyId='policy',
+        policyType='Memo',
+        policyIssuanceYear='12/20/2001',
+        policySunset='NA',
+        issuingBody='IBIB'
+    )
+
     policy_proc = import_reqs.PolicyProcessor()
     policy = policy_proc.from_row(row)
     assert policy.policy_number == 123
@@ -158,10 +186,7 @@ def test_policy_from_row():
 @pytest.mark.django_db
 def test_policy_from_row_duplicate():
     assert Policy.objects.count() == 0
-    row = {'policyNumber': '123', 'policyTitle': 'title 1',
-           'uriPolicyId': 'http://example.com/a', 'ombPolicyId': 'policy',
-           'policyType': 'Memo', 'policyIssuanceYear': '12/20/2001',
-           'policySunset': 'NA', 'issuingBody': 'IBIB'}
+    row = policy_row_factory(policyNumber='123', policyTitle='title 1')
     import_reqs.PolicyProcessor().from_row(row)
     assert Policy.objects.count() == 1
     assert Policy.objects.get(policy_number=123).title == 'title 1'
@@ -205,10 +230,7 @@ def test_repeat_row():
 @pytest.mark.django_db
 def test_no_matching_policy_type():
     """If the policy type isn't found, raise an exception"""
-    row = {'policyNumber': '123', 'policyTitle': 'title 1',
-           'uriPolicyId': 'http://example.com/a', 'ombPolicyId': 'policy',
-           'policyType': 'IAmAnOutlier', 'policyIssuanceYear': '12/20/2001',
-           'policySunset': 'NA', 'issuingBody': 'IBIB'}
+    row = policy_row_factory(policyType='IAmAnOutlier')
     policy_proc = import_reqs.PolicyProcessor()
     with pytest.raises(ValueError):
         policy_proc.from_row(row)
@@ -249,14 +271,9 @@ def test_varying_policy_headers(opfid_field, uri_field):
     Test that we can handle a variety of names for fields (in this case, the
     OMB Policy ID field).
     """
-    row = {
-        'issuingBody': 'IBIB',
-        'policyIssuanceYear': '12/20/2001',
-        'policyNumber': '123',
-        'policySunset': 'NA',
-        'policyTitle': 'title 1',
-        'policyType': 'Guidance',
-    }
+    row = policy_row_factory(policyNumber='123')
+    del row['uriPolicyId']
+    del row['ombPolicyId']
     row[opfid_field] = "some identifier"
     row[uri_field] = "http://example.com/a"
     policy_proc = import_reqs.PolicyProcessor()
