bd440926492e8ea853a68dd7f2552a7f5670fb53
Theresa Summa Lurie <theresa.summa@gsa.gov>
2017-06-18 17:39:30 -0700

    add mobile filter toggle styling

--- ui/components/search-filter-view.jsx
+++ ui/components/search-filter-view.jsx
@@ -9,6 +9,13 @@ export default class SearchFilterView extends React.Component {
     this.openFilters = this.openFilters.bind(this);
   }
 
+  arrowClasses() {
+    const defaultClass = "open-close-indicator";
+    const openClass = defaultClass + " open";
+
+    return (this.state.isOpened ? openClass : defaultClass);
+  }
+
   openFilters() {
     this.setState(
       {isOpened: !this.state.isOpened}
@@ -18,10 +25,10 @@ export default class SearchFilterView extends React.Component {
   render() {
     return (
       <div className="clearfix">
-        <div className="mobile-filters md-hide lg-hide" onClick={this.openFilters} role="link">
+        <a className="mobile-filters md-hide lg-hide" onClick={this.openFilters} role="link">
           Edit Filters
-          <div className="open-close-indicator" />
-        </div>
+          <div className={this.arrowClasses()} />
+        </a>
         <Collapse isOpened={this.state.isOpened} fixedHeight={180}>
           <div className="sidebar sm-col sm-col-12 md-col-2 lg-col-2 p2 no-print">
             Search and filter
@@ -29,7 +36,7 @@ export default class SearchFilterView extends React.Component {
             <div className="show-results" role="link" onClick={this.openFilters}>Show results</div>
           </div>
         </Collapse>
-        <div className="main col col-10 pl4 border-left max-width-3">
+        <div className="main sm-col sm-col-12 md-col-10 lg-col-10 pl4 border-left max-width-3">
           <div className="tab-container no-print">
             <span className="mr4">View:</span>
             <ul className="organize-tabs list-reset inline-block">
