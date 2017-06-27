39e89a8d452fe18d95014410d3d228e2e49e69b1
Micah Taylor <micah.taylor@gsa.gov>
2017-06-27 17:35:00 -0500

    Beta dropdown and footer links

--- ui/components/disclaimer.jsx
+++ ui/components/disclaimer.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 
 const Disclaimer = () => (
   <div className="overflow-auto">
-    <div className="flex items-center usa-disclaimer">
-      <img
-        className="pr1"
-        alt="US flag"
-        width="20"
-        height="13"
-        src="/static/img/usa-flag.png"
-      />
+    <div className="flex items-center justify-between usa-disclaimer">
       <div>
+        <img
+          className="pr1"
+          alt="US flag"
+          width="20"
+          height="13"
+          src="/static/img/usa-flag.png"
+        />
         An official website of the United States government
+      </div>
+      <div>
+        This site is currently in beta. See more &#9660;
       </div>
     </div>
   </div>
