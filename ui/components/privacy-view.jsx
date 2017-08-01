bc7b279e44702223e7e994c10e9b8031e1452caf
CM Lubinski <christopher.lubinski@gsa.gov>
2017-08-01 14:25:41 -0400

    Add skeleton for privacy policy.

---
+++ ui/components/privacy-view.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function PrivacyView() {
+  return (
+    <div>
+      <h2 className="h1">Privacy</h2>
+      <p>Everyone needs privacy</p>
+      <hr />
+      <p>And then some more content</p>
+    </div>
+  );
+}
