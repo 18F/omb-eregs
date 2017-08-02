e7fa6da41e604eca121934cf8bd9a4dadc019254
Micah Taylor <micah.taylor@gsa.gov>
2017-08-01 21:09:53 -0500

    Content changes and mailto links

--- ui/components/privacy-view.jsx
+++ ui/components/privacy-view.jsx
@@ -4,7 +4,7 @@ export default function PrivacyView() {
   return (
     <div className="ml3">
       <h2 className="h1">Privacy Policy</h2>
-      <h3>PROTECTING PRIVACY AND SECURITY</h3>
+      <h3 className="caps">Protecting privacy and security</h3>
 
       <p className="max-width-3">
         {/*eslint-disable */}
@@ -36,32 +36,6 @@ export default function PrivacyView() {
         {/*eslint-enable */}
       </p>
 
-      <h4>How the OMB uses cookies on the OMB policy library</h4>
-      <p className="max-width-3">
-        {/*eslint-disable */}
-        When you visit a website, its server may generate a piece of text known as a “cookie” to place on your device.
-         The cookie, which is unique to your browser, allows the server to “remember” specific information about your visit while you are connected.
-
-        There are two types of cookies — single session (temporary) and multi-session (persistent).
-         Single session cookies last only as long as your Web browser is open.
-         Once you close your browser, the session cookie disappears.
-         Persistent cookies are stored on your device for longer periods.
-         Both types of cookies create an identifier that is unique to your device.
-        The Office of Management and Budget Memorandum M-10-22, Guidance for Online Use of Web Measurement and Customization Technologies, allows Federal agencies to use both session and persistent cookies to improve the delivery of services.
-
-        Session Cookies: We use session cookies for technical purposes, such as to allow better navigation through our site.
-         These session cookies are set by GSA or a contractor acting on GSA’s behalf and let the server hosting the OMB Policy Library know that you are continuing a visit to our site.
-         Our use of session cookies qualifies as “Usage Tier 1—Single Session,” as defined in the OMB M-10-22 guidance.
-        Persistent Cookies: We use persistent cookies to understand the differences between new and returning visitors to the OMB Policy Library.
-        Persistent cookies remain on your device between visits to our site until they expire or are removed by the user.
-         As discussed below under “Google Analytics,” our use of persistent cookies qualifies as “Usage Tier 2—Multi-session without personally identifiable information,” as defined in the OMB M-10-22 guidance.
-         The policy states, “This tier encompasses any use of multi-session Web measurement and customization technologies when no [personally identifiable information] is collected.” We do not use persistent cookies to collect personally identifiable information.
-        If you do not want to accept cookies, you can edit your browser’s options to stop accepting persistent cookies or to prompt you before accepting a cookie from the websites you visit.
-         <a href="https://www.usa.gov/optout-instructions">Here are instructions for how you can disable cookies and/or Google Demographic and Interests reports.</a>
-         {/*eslint-enable */}
-      </p>
-
-
       <h4>Google Analytics</h4>
       <p className="max-width-3">
         {/*eslint-disable */}
@@ -80,8 +54,7 @@ export default function PrivacyView() {
 
         Google Analytics does not collect personally identifiable information through its cookie(s).
          The program does not track individuals and anonymizes the IP addresses of visitors.
-        <a href="https://www.digitalgov.gov/services/dap/common-questions-about-dap-faq/">Common Questions about DAP (FAQ) provides more information about how Google Analytics handles IP addresses.</a>
-         According to GSA’s Common Questions About DAP, “none of the federal government data tracked as part of the Data Analytics Program will be shared with or available to Google’s corporate advertising partners.”
+        <a href="https://www.digitalgov.gov/services/dap/common-questions-about-dap-faq/">Common Questions about DAP (FAQ) provides more information about how Google Analytics handles IP addresses.</a> According to GSA’s Common Questions About DAP, “none of the federal government data tracked as part of the Data Analytics Program will be shared with or available to Google’s corporate advertising partners.”
 
         A limited number of authorized individuals will have user accounts that will allow them to log in to the Google Analytics dashboard and view or run reports regarding visits to the OMB Policy Library and the other web metrics available from the DAP.
 
@@ -95,7 +68,7 @@ export default function PrivacyView() {
       <h4>Information you choose to give the OMB</h4>
       <p className="max-width-3">
         {/*eslint-disable */}
-        Users of this website may send the OMB feedback or report an issue by sending an email to ofcio@omb.eop.gov.
+        Users of this website may send the OMB feedback or report an issue by sending an email to <a href="mailto:ofcio@omb.eop.gov">ofcio@omb.eop.gov</a>.
         If you choose to send us your personally identifiable information, we will only use that information to respond to your message.
          We only share the information you give us with another government agency if your question relates to that agency, or as otherwise required by law.
          The OMB Policy Library never collects information or creates individual profiles for the purposes of advertising, marketing, or any other commercial purpose.
@@ -111,32 +84,14 @@ export default function PrivacyView() {
         We believe in the importance of protecting the privacy of children online.
          The Children’s Online Privacy Protection Act (COPPA) governs information gathered online from or about children under the age of 13.
          This site is not intended to solicit or collection information of any kind from children under age 13.
-         If you believe that we have received information from a child under age 13, please contact us at ofcio@omb.eop.gov.
+         If you believe that we have received information from a child under age 13, please contact us at <a href="mailto:ofcio@omb.eop.gov">ofcio@omb.eop.gov</a>.
          {/*eslint-enable */}
       </p>
-
-
-      <h4>Security</h4>
-      <p className="max-width-3">
-        {/*eslint-disable */}
-        This website was built using Sites, a government-hosted service designed to enable the rapid deployment of government websites in a secure and readily accessible environment.
-        Sites is a product of the Office of Products and Programs.
-         General Services Administration.
-         Sites, which hosts the OMB Policy Library, provides a secure, FISMA compliant platform.
-
-        The security policy of Sites states, “[f]or site security purposes and to ensure that this service remains available to all users, this government computer system employs software programs to monitor network traffic to identify unauthorized attempts to upload or change information, or otherwise cause damage.”
-
-        In addition, the security policy of Sites provides that “[u]nauthorized attempts to upload information or change information on this service are strictly prohibited and may be punishable under the Computer Fraud and Abuse Act of 1986 and the National Information Infrastructure Protection Act.”
-
-        We encourage you to visit Sites if you have additional questions about the government-hosted service.
-        {/*eslint-enable */}
-      </p>
-
 
       <h4>Questions about the privacy policy</h4>
       <p className="max-width-3">
         {/*eslint-disable */}
-        If you have questions about this privacy policy or the privacy practices of the OMB, please submit your questions to ofcio@omb.eop.gov.
+        If you have questions about this privacy policy or the privacy practices of the OMB, please submit your questions to <a href="mailto:ofcio@omb.eop.gov">ofcio@omb.eop.gov</a>.
         {/*eslint-enable */}
       </p>
 
