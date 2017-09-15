8426326e8a10aadb10296d407e755b9ce81136c6
CM Lubinski <christopher.lubinski@gsa.gov>
2017-09-15 18:20:37 -0400

    Add spaces to comments.
    The latest versions of eslint catch these issues better, so our builds are
    failing.

--- ui/components/privacy-view.jsx
+++ ui/components/privacy-view.jsx
@@ -7,17 +7,17 @@ export default function PrivacyView() {
       <h3 className="caps">Protecting privacy and security</h3>
 
       <p className="max-width-3">
-        {/*eslint-disable */}
+        {/* eslint-disable */}
         Protecting the privacy and security of individuals’ personal information is very important to us.
         We do not collect any information that directly identifies you when you visit the OMB Policy Library unless you choose to provide that information by contacting us.
          However, the website may collect a limited amount of information about your visit for the purposes of website analytics and customization.
          Please read this notice to understand what we do with the limited amount of information about your visit that we may collect.
-         {/*eslint-enable */}
+         {/* eslint-enable */}
       </p>
 
       <h4>Information collected and stored automatically</h4>
       <p className="max-width-3">
-        {/*eslint-disable */}
+        {/* eslint-disable */}
         As with many Federal Government websites, the OMB Policy Library is hosted by the U.S.
          General Services Administration (GSA).
         GSA, or contractors working on behalf of GSA, collect limited information about visits to the OMB Policy Library.
@@ -33,12 +33,12 @@ export default function PrivacyView() {
         Internet Protocol Address;
         the Internet address of the website you came from if it linked you directly to the OMB Policy Library; and
         search terms that you enter into the the OMB Policy Library search tool.
-        {/*eslint-enable */}
+        {/* eslint-enable */}
       </p>
 
       <h4>Google Analytics</h4>
       <p className="max-width-3">
-        {/*eslint-disable */}
+        {/* eslint-disable */}
         The OMB Policy Library participates in the U.S.
          Digital Analytics Program, (DAP) which utilizes a unified Google Analytics account for Federal Government agencies.
          This program helps Federal agencies understand how people find, access, and use government services online.
@@ -62,12 +62,12 @@ export default function PrivacyView() {
         While the details vary from browser to browser, most modern browsers can be set up to accept, reject, or request user intervention when a site asks to set a cookie.
 
         You can view web metrics information at <a href="https://analytics.usa.gov/">analytics.usa.gov</a>.
-        {/*eslint-enable */}
+        {/* eslint-enable */}
       </p>
 
       <h4>Information you choose to give the OMB</h4>
       <p className="max-width-3">
-        {/*eslint-disable */}
+        {/* eslint-disable */}
         Users of this website may send the OMB feedback or report an issue by sending an email to <a href="mailto:ofcio@omb.eop.gov">ofcio@omb.eop.gov</a>.
         If you choose to send us your personally identifiable information, we will only use that information to respond to your message.
          We only share the information you give us with another government agency if your question relates to that agency, or as otherwise required by law.
@@ -75,24 +75,24 @@ export default function PrivacyView() {
           We do not create or store lists of email addresses or other information to deliver email alerts, newsletters, or otherwise send marketing messages or contact individuals.
            When you contact us, any personally identifiable information you provide is voluntary.
            Please do not include sensitive personally identifiable information or other sensitive information in the content of your email.
-           {/*eslint-enable */}
+           {/* eslint-enable */}
       </p>
 
       <h4>Children and privacy on the OMB policy library</h4>
       <p className="max-width-3">
-        {/*eslint-disable */}
+        {/* eslint-disable */}
         We believe in the importance of protecting the privacy of children online.
          The Children’s Online Privacy Protection Act (COPPA) governs information gathered online from or about children under the age of 13.
          This site is not intended to solicit or collection information of any kind from children under age 13.
          If you believe that we have received information from a child under age 13, please contact us at <a href="mailto:ofcio@omb.eop.gov">ofcio@omb.eop.gov</a>.
-         {/*eslint-enable */}
+         {/* eslint-enable */}
       </p>
 
       <h4>Questions about the privacy policy</h4>
       <p className="max-width-3">
-        {/*eslint-disable */}
+        {/* eslint-disable */}
         If you have questions about this privacy policy or the privacy practices of the OMB, please submit your questions to <a href="mailto:ofcio@omb.eop.gov">ofcio@omb.eop.gov</a>.
-        {/*eslint-enable */}
+        {/* eslint-enable */}
       </p>
 
     </div>
