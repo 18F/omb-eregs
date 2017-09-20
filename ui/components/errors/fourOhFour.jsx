07f7be5c28ba2a701ebc3d57cce5518df38c80ec
CM Lubinski <christopher.lubinski@gsa.gov>
2017-09-20 12:57:15 -0400

    Update 404 page styles.
    By reusing some of the styles from the homepage, we get the gold-bordered
    text, a star-based divider, and similar font choices. This also adds a
    component for button-like links.

--- ui/components/errors/fourOhFour.jsx
+++ ui/components/errors/fourOhFour.jsx
@@ -9,8 +9,22 @@ import Html from '../html';
 export default () => (
   <Html allowDynamic={false}>
     <App>
-      <h1>Page not found</h1>
-      <p>Please check the URL and try again</p>
+      <section className="py3">
+        <div className="landing-section gold-border pb2">
+          <h1 className="h2">
+            We can&rsquo;t find the page you&rsquo;re looking for.
+          </h1>
+          <p className="content">
+            Visit our <a href="/">homepage</a>
+            {' '}or <a href="mailto:ofcio@omb.eop.gov">contact us</a>
+            {' '}if you need additional help.
+          </p>
+          <div className="my3">
+            <a className="button-like" href="/">Return home</a>
+          </div>
+        </div>
+        <hr className="stars-divider" />
+      </section>
     </App>
   </Html>
 );
