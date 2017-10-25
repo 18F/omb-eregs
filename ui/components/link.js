90fb21fe6a0e0df7ff89ee7520f30d98bc1033d9
CM Lubinski <christopher.lubinski@gsa.gov>
2017-10-25 12:43:59 -0400

    Simplify Link logic.
    We were taking a whitelist approach and only passing approved parameters (in
    this case, only className and aria props). Rather than keep track (and test)
    all of that logic, let's just pass the properties along to the anchor tag.

--- ui/components/link.js
+++ ui/components/link.js
@@ -3,52 +3,36 @@ import React from 'react';
 
 import { Link as RouterLink, routes } from '../routes';
 
-function getAriaProps(props) {
-  const ariaProps = Object.keys(props).filter(name => name.match(/^aria-/));
-  const otherProps = ariaProps.map(name => ({ name, value: props[name] }))
-    .reduce((accum, next) => ({
-      ...accum,
-      [next.name]: next.value,
-    }), {});
-
-  return otherProps;
-}
-
-export default function Link({ children, className, href, params, route, ...props }) {
-  const ariaProps = getAriaProps(props);
+export default function Link({ children, href, params, route, ...otherProps }) {
   if (route) {
     return (
       <RouterLink params={params} route={route}>
-        <a className={className} {...ariaProps}>{children}</a>
+        <a {...otherProps}>{children}</a>
       </RouterLink>
     );
   } else if (href) {
-    const attrs = href.search(/^https|http|^\/\//) === 0 ? {
-      target: '_blank',
-      rel: 'noopener noreferrer',
-      'aria-label': 'Link opens in a new window',
-    } : {};
-    return (
-      <a className={className} href={href} {...attrs} {...ariaProps} >
-        {children}
-      </a>
-    );
+    const props = {
+      ...otherProps,
+      href,
+    };
+    if (href.search(/^https|http|^\/\//) === 0) {
+      props.target = '_blank';
+      props.rel = 'noopener noreferrer';
+      props['aria-label'] = 'Link opens in a new window';
+    }
+    return <a {...props}>{children}</a>;
   }
 }
 
 const existingRoutes = routes.map(r => r.name);
-/* eslint-disable consistent-return, react/forbid-prop-types */
 Link.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string,
   href: PropTypes.string,
-  params: PropTypes.object,
+  params: PropTypes.shape({}),
   route: PropTypes.oneOf([null, ...existingRoutes]),
 };
-/* eslint-enable consistent-return, react/forbid-prop-types */
 
 Link.defaultProps = {
-  className: '',
   href: null,
   params: {},
   route: null,
