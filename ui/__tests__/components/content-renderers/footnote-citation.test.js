af401b456a1e3a41fed4dbcad55d0cfacfecea27
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-11-08 17:43:55 -0500

    Prevent styling from breaking around improperly nested divs inside paragraphs

--- ui/__tests__/components/content-renderers/footnote-citation.test.js
+++ ui/__tests__/components/content-renderers/footnote-citation.test.js
@@ -11,10 +11,18 @@ describe('<FootnoteCitation />', () => {
     expect(text).toMatch(/Some text here /);
     expect(text).toMatch(/Footnote /);
   });
+
   it('references the footnote node', () => {
     const content = { footnote_node: 'aaa_1__bbb_2', text: '' };
     const html = mount(<FootnoteCitation content={content} />).html();
     expect(html).toMatch(/aaa_1__bbb_2/);
+  });
+
+  it('expands on click', () => {
+    const content = { footnote_node: 'aaa_1__bbb_2', text: '' };
+    const footnote = mount(<FootnoteCitation content={content} />);
+    footnote.find('a').simulate('click');
+    expect(footnote.html()).toMatch(/active footnote-link/);
   });
 });
 
