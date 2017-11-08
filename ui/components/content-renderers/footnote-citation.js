ec2a148dd5ab300a430b4527c66eda7e71c89579
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-11-07 21:45:23 -0500

    Merge upstream

--- ui/components/content-renderers/footnote-citation.js
+++ ui/components/content-renderers/footnote-citation.js
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import renderNode from '../../util/render-node';
+
 import Link from '../link';
 
+/* eslint-disable */
+const demoData = {"children":[],"content":[{"content_type":"__text__","text":"The FDCCI was first established by OMB â€œMemo for CIOs: Federal Data\n        Center Consolidation Initiative,â€ issued on February 26, 2010, and\n        modified by subsequent memoranda."}],"depth":3,"identifier":"policy_1__sec_1__para_1__footnote_1","marker":"1","node_type":"footnote","requirement":null,"text":"The FDCCI was first established by OMB â€œMemo for CIOs: Federal Data\n        Center Consolidation Initiative,â€ issued on February 26, 2010, and\n        modified by subsequent memoranda.","type_emblem":"1","policy":{"issuance":"2016-08-01","omb_policy_id":"M-16-19","original_url":"https://www.whitehouse.gov/sites/default/files/omb/memoranda/2016/m_16_19_1.pdf","title":"Data Center Optimization Initiative (DCOI)"}};
+/* eslint-enable */
 
-export default function FootnoteCitation({ content }) {
-  const href = `#${content.footnote_node.identifier}`;
-  return <Link className="footnote-link" href={href}><sup>Footnote { content.text }</sup></Link>;
-}
-FootnoteCitation.propTypes = {
+const propTypes = {
   content: PropTypes.shape({
     footnote_node: PropTypes.shape({
       identifier: PropTypes.string.isRequired,
@@ -17,3 +18,50 @@ FootnoteCitation.propTypes = {
   }).isRequired,
 };
 
+export default class FootnoteCitation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+    };
+    this.handleCitationClick = this.handleCitationClick.bind(this);
+  }
+
+  handleCitationClick(e) {
+    e.preventDefault();
+    this.setState({ expanded: !this.state.expanded });
+  }
+
+  renderFootnote() {
+    return renderNode(demoData);
+  }
+
+  render() {
+    let citation;
+    const expanded = this.state.expanded;
+    const klass = expanded ? 'active footnote-link' : 'footnote-link';
+    const href = `#${this.props.content.footnote_node.identifier}`;
+    const link = (
+      <Link onClick={this.handleCitationClick} href={href}>
+        <sup>Footnote { this.props.content.text }</sup>
+      </Link>
+    );
+    if (expanded) {
+      citation = (
+        <span>
+          { this.renderFootnote() }
+          <span className="clearfix"></span>
+        </span>
+      );
+    }
+    return (
+      <cite className={klass} data-citation-target={href}>
+        { link }
+        { citation }
+      </cite>
+    );
+  }
+}
+
+FootnoteCitation.propTypes = propTypes;
