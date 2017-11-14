e025edb84ba76faabd61a904517054be6dfd0e17
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2017-11-13 16:12:05 -0800

    Add narrower math test.

--- ui/__tests__/components/node-renderers/math.test.js
+++ ui/__tests__/components/node-renderers/math.test.js
@@ -12,12 +12,12 @@ describe('<TeXMath />', () => {
   it('generates something math-like', () => {
     const docNode = {
       identifier: 'aaa_1__bbb_2__ccc_3_math',
-      text: '\\frac{10}{11}',
+      text: '\\frac{12345654321}{10987678901}',
     };
     const result = mount(<TeXMath docNode={docNode} />);
     expect(result.render().find('span.katex')).toHaveLength(1);
-    expect(result.html()).toContain('10');
-    expect(result.html()).toContain('11');
+    expect(result.text()).toContain('12345654321');
+    expect(result.text()).toContain('10987678901');
   });
 });
 
