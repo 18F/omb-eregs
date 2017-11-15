32234fe5a3ee7c32f7aebf5677c661aeceacdb83
CM Lubinski <christopher.lubinski@gsa.gov>
2017-11-15 10:16:51 -0500

    Add comment explaining limited state.
    Thanks for the suggestion @toolness!

--- ui/store/initial-state.js
+++ ui/store/initial-state.js
@@ -1,1 +1,3 @@
+/* Note that we're only tracking which foonote is opened right now. We may
+ * expand this to include the url and other state vars in the future. */
 export default { openedFootnote: '' };
