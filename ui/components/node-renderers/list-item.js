e57670c0a45afebae1260241c3595f90bd055ffb
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-11-15 22:42:29 -0500

    Increase the size of the bullets and prevent wrapping under bullet

--- ui/components/node-renderers/list-item.js
+++ ui/components/node-renderers/list-item.js
@@ -4,8 +4,15 @@ import React from 'react';
 import renderNode from '../../util/render-node';
 
 export default function ListItem({ docNode }) {
+  let markerType;
+  if (docNode.marker == "•") {
+    markerType = "list-style-bullet";
+  } else if (docNode.marker.match(/\d+/g) != null) {
+    markerType = "list-style-number";
+  }
+
   return (
-    <li className="node-list-item" id={docNode.identifier}>
+    <li className={"node-list-item " + markerType} id={docNode.identifier}>
       <span className="list-item-marker">{ docNode.marker }</span>
       <div className="list-item-text">{ docNode.children.map(renderNode) }</div>
     </li>
