984df4145368fd9a9a8de1cd4fce59cc63cf2168
Atul Varma <varmaa@gmail.com>
2017-11-24 17:01:20 -0500

    Move tests into separate tests dir.

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+from ombpdf.download_pdfs import download
+from ombpdf import util, document
+
+
+pages_cache = {}
+
+
+def read_pages(relpath):
+    if relpath not in pages_cache:
+        with download(relpath).open('rb') as fp:
+            pages_cache[relpath] = util.get_ltpages(fp)
+    return pages_cache[relpath]
+
+
+@pytest.fixture
+def m_16_19_pages():
+    return read_pages('2016/m_16_19_1.pdf')
+
+
+@pytest.fixture
+def m_16_19_doc(m_16_19_pages):
+    return document.OMBDocument(m_16_19_pages)
---
+++ tests/test_footnotes.py
@@ -0,0 +1,24 @@
+from ombpdf import footnotes
+
+
+def test_footnotes_annotate_citations_works(m_16_19_doc):
+    citations = footnotes.annotate_citations(m_16_19_doc)
+    assert citations[:3] == [
+        (1, 'shift IT investments to more efficient computing '
+            'platforms and technologies.'),
+        (2, 'Acquisition Reform Act (FITARA),'),
+        (3, '(OFCIO)'),
+    ]
+
+
+def test_footnotes_annotate_footnotes_works(m_16_19_doc):
+    notes = footnotes.annotate_footnotes(m_16_19_doc)
+    assert notes[:1] == [
+        (1, 'The FDCCI was first established by OMB “Memo for CIOs: '
+            'Federal Data Center Consolidation Initiative,” issued '
+            'on February 26, 2010, and modified by subsequent memoranda. '),
+    ]
+
+
+def test_footnotes_main_works(m_16_19_doc):
+    footnotes.main(m_16_19_doc)
