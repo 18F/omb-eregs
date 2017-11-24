0c8569e3af583d91c0b50e85d300c35aed9a9dcb
Atul Varma <varmaa@gmail.com>
2017-11-24 11:50:37 -0500

    Parse footnotes.

--- ombpdf/footnotes.py
+++ ombpdf/footnotes.py
@@ -10,6 +10,8 @@ from .fontsize import FontSize
 
 NUMBER_RE = re.compile(r'[0-9]')
 
+FOOTNOTE_RE = re.compile(r'([0-9]+) (.+)')
+
 
 def find_citations(doc):
     citations = []
@@ -38,6 +40,47 @@ def find_citations(doc):
     return citations
 
 
+def for_all_chars(lttextline, fn):
+    return len([
+        ltchar for ltchar in
+        util.iter_flattened_layout(lttextline, layout.LTChar)
+        if not fn(ltchar, FontSize.from_ltchar(ltchar))
+    ]) == 0
+
+
+def find_footnotes(doc):
+    footnotes = []
+    curr_footnote = None
+
+    def finish_footnote():
+        nonlocal curr_footnote
+
+        if curr_footnote:
+            footnotes.append(tuple(curr_footnote))
+        curr_footnote = None
+
+    for line in doc.iter_flattened(layout.LTTextLineHorizontal):
+        only_small_chars = for_all_chars(
+            line,
+            lambda _, fs: fs.size < doc.paragraph_fontsize.size
+        )
+        if only_small_chars:
+            chars = line.get_text()
+            match = FOOTNOTE_RE.match(chars)
+            if match:
+                finish_footnote()
+                footnote, desc = match.groups()
+                curr_footnote = [int(footnote), desc]
+            elif curr_footnote:
+                curr_footnote[1] += chars
+        else:
+            finish_footnote()
+    finish_footnote()
+    return footnotes
+
+
 if __name__ == "__main__":
     with open(sys.argv[1], 'rb') as infile:
-        print(find_citations(OMBDocument.from_file(infile)))
+        doc = OMBDocument.from_file(infile)
+        print(find_citations(doc))
+        print(find_footnotes(doc))
