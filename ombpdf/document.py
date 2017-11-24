c37c03eba0c5d879a048bbb737614e1b3e5093d0
Atul Varma <varmaa@gmail.com>
2017-11-24 12:26:49 -0500

    Factor out OMBTextLine, OMBTextCharacter classes.

--- ombpdf/document.py
+++ ombpdf/document.py
@@ -1,3 +1,5 @@
+from pdfminer import layout
+
 from . import fontsize
 from . import util
 
@@ -7,6 +9,10 @@ class OMBDocument:
         stats = fontsize.get_font_size_stats(ltpages)
         self.paragraph_fontsize = stats.most_common(1)[0][0]
         self.ltpages = ltpages
+        self.lines = [
+            OMBTextLine(line)
+            for line in self.iter_flattened(layout.LTTextLineHorizontal)
+        ]
 
     def iter_flattened(self, class_filter=object):
         return util.iter_flattened_layout(self.ltpages, class_filter)
@@ -14,3 +20,23 @@ class OMBDocument:
     @classmethod
     def from_file(cls, fp):
         return cls(util.get_ltpages(fp))
+
+
+class OMBTextCharacter(str):
+    def __new__(cls, ltchar):
+        obj = str.__new__(cls, ltchar.get_text())
+        obj.ltchar = ltchar
+        obj.fontsize = fontsize.FontSize.from_ltchar(ltchar)
+        return obj
+
+
+class OMBTextLine(list):
+    def __init__(self, lttextline):
+        super().__init__([
+            OMBTextCharacter(ltchar) for ltchar
+            in util.iter_flattened_layout(lttextline, layout.LTChar)
+        ])
+        self.lttextline = lttextline
+
+    def __str__(self):
+        return ''.join([char for char in self])
--- ombpdf/footnotes.py
+++ ombpdf/footnotes.py
@@ -15,17 +15,15 @@ FOOTNOTE_RE = re.compile(r'([0-9]+) (.+)')
 
 def find_citations(doc):
     citations = []
-    for line in doc.iter_flattened(layout.LTTextLineHorizontal):
+    for line in doc.lines:
         prev_paragraph_chars = ''
         curr_citation = ''
 
         def add_citation():
-            citations.append((curr_citation, prev_paragraph_chars))
+            citations.append((int(curr_citation), prev_paragraph_chars))
 
-        for ltchar in util.iter_flattened_layout(line, layout.LTChar):
-            char = ltchar.get_text()
-            fs = FontSize.from_ltchar(ltchar)
-            if fs.size < doc.paragraph_fontsize.size:
+        for char in line:
+            if char.fontsize.size < doc.paragraph_fontsize.size:
                 if NUMBER_RE.match(char):
                     if prev_paragraph_chars:
                         curr_citation += char
@@ -40,14 +38,6 @@ def find_citations(doc):
     return citations
 
 
-def for_all_chars(lttextline, fn):
-    return len([
-        ltchar for ltchar in
-        util.iter_flattened_layout(lttextline, layout.LTChar)
-        if not fn(ltchar, FontSize.from_ltchar(ltchar))
-    ]) == 0
-
-
 def find_footnotes(doc):
     footnotes = []
     curr_footnote = None
@@ -59,13 +49,13 @@ def find_footnotes(doc):
             footnotes.append(tuple(curr_footnote))
         curr_footnote = None
 
-    for line in doc.iter_flattened(layout.LTTextLineHorizontal):
-        only_small_chars = for_all_chars(
-            line,
-            lambda _, fs: fs.size < doc.paragraph_fontsize.size
-        )
-        if only_small_chars:
-            chars = line.get_text()
+    for line in doc.lines:
+        big_chars = [
+            char for char in line
+            if char.fontsize.size >= doc.paragraph_fontsize.size
+        ]
+        if not big_chars:
+            chars = str(line)
             match = FOOTNOTE_RE.match(chars)
             if match:
                 finish_footnote()
