ddf38c5555f2c406e2e6866509aecd8e1b645b82
CM Lubinski <christopher.lubinski@gsa.gov>
2017-11-28 18:44:20 -0500

    Add a filetype-based icon for external links.

--- ui/__tests__/components/content-renderers/external-link.test.js
+++ ui/__tests__/components/content-renderers/external-link.test.js
@@ -15,7 +15,7 @@ describe('<ExternalLink />', () => {
       expect(result.prop('href')).toBe('http://example.com/aaa');
     });
     it('includes the content text', () => {
-      expect(result.children().text()).toBe('aLink');
+      expect(result.children().first().text()).toBe('aLink');
     });
   });
 
@@ -24,7 +24,7 @@ describe('<ExternalLink />', () => {
       const content = { href: 'http://example.com/aaa', text: 'not-that-url' };
       const result = shallow(<ExternalLink content={content} />);
 
-      expect(result.prop('className')).toBe('print-url');
+      expect(result.hasClass('print-url')).toBe(true);
     });
 
     it('is not present when the link url is obvious', () => {
@@ -34,7 +34,25 @@ describe('<ExternalLink />', () => {
       };
       const result = shallow(<ExternalLink content={content} />);
 
-      expect(result.prop('className')).toBe('');
+      expect(result.hasClass('print-url')).toBe(false);
+    });
+  });
+
+  describe('icon', () => {
+    [
+      ['http://example.com', 'fa-external-link'],
+      ['http://example.com/', 'fa-external-link'],
+      ['http://example.com/page', 'fa-external-link'],
+      ['http://example.com/page/', 'fa-external-link'],
+      ['http://example.com/page.htm', 'fa-external-link'],
+      ['http://example.com/page.html', 'fa-external-link'],
+      ['http://example.com/page.pdf', 'fa-file-o'],
+      ['http://example.com/page.html.tgz', 'fa-file-o'],
+    ].forEach(([href, className]) => {
+      it(`works correctly for ${href}`, () => {
+        const rendered = shallow(<ExternalLink content={{ href, text: '' }} />);
+        expect(rendered.find('.fa').hasClass(className)).toBe(true);
+      });
     });
   });
 });
