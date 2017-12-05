a98ae0d84a4a45312811377e938fcf1112baead4
Atul Varma <varmaa@gmail.com>
2017-12-05 15:34:31 -0500

    Add some TODOs.

--- ombpdf/webapp/js/main.js
+++ ombpdf/webapp/js/main.js
@@ -1,8 +1,10 @@
 var pdfjsLib = require('pdfjs-dist');
 
+// TODO: Get this from the current page's DOM.
 var pdfPath = '/raw/2016/m_16_19_1.pdf';
 
 // Setting worker path to worker bundle.
+// TODO: Get this from the current page's DOM.
 pdfjsLib.PDFJS.workerSrc = '/static/js/pdf.worker.bundle.js';
 
 // Loading a document.
@@ -13,6 +15,7 @@ loadingTask.promise.then(function (pdfDocument) {
   var canvas = document.createElement('canvas');
   firstPage.prepend(canvas);
 
+  // TODO: Do this for all pages.
   return pdfDocument.getPage(1).then(function (pdfPage) {
     // Display page on the existing canvas with 100% scale.
     var viewport = pdfPage.getViewport(1.0);
