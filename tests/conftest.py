aa64a543bb807f80564a43c421d58044e7a37abb
Atul Varma <varmaa@gmail.com>
2017-12-06 11:56:11 -0500

    Realign lines past left edge instead of culling them. Fixes #29.

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,16 @@ def read_pages(relpath):
 
 
 @pytest.fixture
+def m_11_29_pages():
+    return read_pages('2011/m11-29.pdf')
+
+
+@pytest.fixture
+def m_11_29_doc(m_11_29_pages):
+    return document.OMBDocument(m_11_29_pages, filename='m11-29.pdf')
+
+
+@pytest.fixture
 def m_16_19_pages():
     return read_pages('2016/m_16_19_1.pdf')
 
