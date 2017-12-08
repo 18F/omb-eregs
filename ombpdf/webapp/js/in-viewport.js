dc5a50c21205dec3957a0035c2e4b89ead740635
Atul Varma <varmaa@gmail.com>
2017-12-08 09:36:51 -0500

    Split up main.js into multiple files.

---
+++ ombpdf/webapp/js/in-viewport.js
@@ -0,0 +1,27 @@
+// This script sets the `in-viewport` class on all pages to reflect
+// whether the page is currently in the browser's viewport. Combined
+// with CSS that sets `display: none` on anything not in the viewport,
+// this significantly speeds up rendering on Chrome.
+
+var pages = document.querySelectorAll('.page');
+
+function isElPartlyInViewport (el) {
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.bottom >= 0 &&
+    rect.right >= 0 &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function setInViewport() {
+  pages.forEach(function(page, i) {
+    page.classList.toggle('in-viewport', isElPartlyInViewport(page));
+  });
+}
+
+['scroll', 'resize', 'load'].forEach(function(name) {
+  window.addEventListener(name, setInViewport);
+});
