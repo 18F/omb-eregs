94300002c11c70b295c3b70558c2fce48dbb9180
Atul Varma <varmaa@gmail.com>
2017-12-11 16:48:50 -0500

    Make the webapp runnable from the '/ombpdf' dir.

--- ombpdf/webpack.config.js
+++ ombpdf/webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
   context: __dirname,
   watch: true,
   entry: {
-    'main': './ombpdf/webapp/js/main.js',
+    'main': './webapp/js/main.js',
     'pdf.worker': 'pdfjs-dist/build/pdf.worker.entry'
   },
   output: {
-    path: path.join(__dirname, 'ombpdf/webapp/static/js/'),
+    path: path.join(__dirname, 'webapp/static/js/'),
     publicPath: 'static/js/',
     filename: '[name].bundle.js'
   }
