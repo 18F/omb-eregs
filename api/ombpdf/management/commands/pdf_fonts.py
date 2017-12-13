c152e7fceb469265405317443899b5125a230d50
Atul Varma <varmaa@gmail.com>
2017-12-13 08:44:02 -0500

    Use argparse.FileType in pdf_fonts command.

--- api/ombpdf/management/commands/pdf_fonts.py
+++ api/ombpdf/management/commands/pdf_fonts.py
@@ -1,3 +1,5 @@
+from argparse import FileType
+
 from django.core.management.base import BaseCommand
 
 import ombpdf.fontsize
@@ -8,12 +10,7 @@ class Command(BaseCommand):
     help = 'Show font usage statistics for a PDF.'  # NOQA
 
     def add_arguments(self, parser):
-        parser.add_argument('filename')
-
-    def get_ltpages(self, filename):
-        with open(filename, 'rb') as infile:
-            return ombpdf.util.get_ltpages(infile)
+        parser.add_argument('filename', type=FileType('rb'))
 
     def handle(self, *args, **options):
-        filename = options['filename']
-        ombpdf.fontsize.main(self.get_ltpages(filename))
+        ombpdf.fontsize.main(ombpdf.util.get_ltpages(options['filename']))
