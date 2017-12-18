84fe58b3edb12b4f79f164feaf8e44ba2d6d9dde
CM Lubinski <christopher.lubinski@gsa.gov>
2017-12-18 09:24:36 -0500

    Define Policy's interface more explicitly.
    Following the construction of the DocumentNode and Meta objects, define
    Policy's fields in its constructor. Prefer camelCase for field names.

--- ui/util/policy.js
+++ ui/util/policy.js
@@ -3,25 +3,30 @@ import PropTypes from 'prop-types';
 /**
  * The Policy class wraps the policy structure returned by the API
  * and adds a number of convenience methods to it.  It's intended to be
- * immutable; no state is stored in it, and calling JSON.stringify() on
- * it yields the same policy structure it was initialized with.
+ * immutable; no state is stored in it.
  */
 
 export default class Policy {
-  constructor(policy) {
-    Object.assign(this, policy);
+  constructor(initial) {
+    this.hasDocnode = initial.hasDocnode || initial.has_docnode || false;
+    this.id = initial.id || '';
+    this.ombPolicyId = initial.ombPolicyId || initial.omb_policy_id || '';
+    this.originalUrl = initial.originalUrl || initial.original_url || '';
+    this.relevantReqs = initial.relevantReqs || initial.relevant_reqs || 0;
+    this.titleWithNumber = initial.titleWithNumber || initial.title_with_number || '';
+    this.totalReqs = initial.totalReqs || initial.total_reqs || 0;
     Object.freeze(this);
   }
 
   hasDocument() {
-    return this.omb_policy_id && this.has_docnode;
+    return this.ombPolicyId && this.hasDocnode;
   }
 
   getDocumentLinkProps() {
     return {
       route: 'document',
       params: {
-        policyId: this.omb_policy_id,
+        policyId: this.ombPolicyId,
       },
     };
   }
