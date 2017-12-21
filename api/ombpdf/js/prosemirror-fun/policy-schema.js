93240cba0608dda0f4174436e2f59d896a3e9962
Atul Varma <varmaa@gmail.com>
2017-12-21 18:40:23 -0500

    Use <section> for sections. Add heading type to schema.

--- api/ombpdf/js/prosemirror-fun/policy-schema.js
+++ api/ombpdf/js/prosemirror-fun/policy-schema.js
@@ -2,18 +2,26 @@ import {Schema} from "prosemirror-model";
 
 export const nodes = {
   doc: {
-    content: "block+",
+    content: "section+",
   },
 
   paragraph: {
     content: 'inline*',
-    group: 'block',
     toDOM() { return ['p', 0]; },
   },
 
   section: {
-    content: 'block+',
-    toDOM() { return ["div", 0]; },
+    content: 'heading? paragraph+',
+    toDOM() { return ["section", {'class': 'doc-section'}, 0]; },
+  },
+
+  heading: {
+    content: 'inline*',
+    // We're assuming the HTML5 outline algorithm here, even though
+    // browsers may or may not actually implement it:
+    //
+    // https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML_sections_and_outlines#The_HTML5_outline_algorithm
+    toDOM() { return ['h1', 0]; },
   },
 
   text: {
