291578f2b8f8af05fede04ea94b8a21c5f939a5c
Atul Varma <varmaa@gmail.com>
2017-12-21 12:41:48 -0500

    Load a document in ProseMirror via the API.

--- api/ombpdf/js/prosemirror-fun/main.js
+++ api/ombpdf/js/prosemirror-fun/main.js
@@ -2,7 +2,7 @@
 
 import {EditorState} from "prosemirror-state";
 import {EditorView} from "prosemirror-view";
-import {Schema, DOMParser} from "prosemirror-model";
+import {Schema, DOMParser, Node, Fragment} from "prosemirror-model";
 import {schema} from "prosemirror-schema-basic";
 import {addListNodes} from "prosemirror-schema-list";
 import {exampleSetup} from "prosemirror-example-setup";
@@ -12,16 +12,101 @@ import styles from 'prosemirror-gapcursor/style/gapcursor.css';
 import styles from 'prosemirror-menu/style/menu.css';
 import styles from 'prosemirror-example-setup/style/style.css';
 
+function addSectionNodes(nodes) {
+  return nodes.append({
+    section: {
+      content: 'block+',
+      parseDOM: [{tag: 'div'}],
+      toDOM() { return ["div", 0]; },
+    }
+  });
+}
+
+function dbDocToProseMirrorDoc(root, schema) {
+  const convert = node => {
+    if (node.node_type) {
+      if (node.node_type in node_type_converters) {
+        return node_type_converters[node.node_type](node);
+      }
+      throw new Error(`Unknown node type: ${node.node_type}`);
+    }
+    if (node.content_type) {
+      if (node.content_type in content_type_converters) {
+        return content_type_converters[node.content_type](node);
+      }
+      throw new Error(`Unknown content type: ${node.content_type}`);
+    }
+    throw new Error(`Don't know what to do with node: ` +
+                    `${JSON.stringify(node)}`);
+  };
+
+  const makeNode = info => {
+    if (!(info.type in schema.nodes)) {
+      throw new Error(`Node type is not in schema: ${info.type}`);
+    }
+
+    return new Node(
+      schema.nodes[info.type],
+      info.attrs || {},
+      new Fragment(info.content, null),
+      info.marks
+    );
+  };
+
+  const node_type_converters = {
+    policy(node) {
+      return makeNode({
+        type: 'doc',
+        content: node.children.map(convert),
+      });
+    },
+    para(node) {
+      return makeNode({
+        type: 'paragraph',
+        content: node.content.map(convert),
+      });
+    },
+    sec(node) {
+      return makeNode({
+        type: 'section',
+        content: node.children.map(convert),
+      });
+    },
+  };
+
+  const content_type_converters = {
+    __text__(node) {
+      return Node.fromJSON(schema, {
+        type: 'text',
+        text: node.text,
+      });
+    }
+  };
+
+  return Promise.resolve(convert(root));
+}
+
+function proseMirrorDocToDbDoc(doc) {
+  // TODO implement this!
+}
+
 const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block", "block"),
+  nodes: addSectionNodes(
+    addListNodes(schema.spec.nodes, "paragraph block", "block")
+  ),
   marks: schema.spec.marks,
 });
 
 const contentEl = document.querySelector('#content');
 
-window.view = new EditorView(document.querySelector('#editor'), {
-  state: EditorState.create({
-    doc: DOMParser.fromSchema(mySchema).parse(contentEl),
-    plugins: exampleSetup({schema: mySchema}),
-  }),
-});
+window.fetch('/document/M-14-10')
+  .then(res => res.json())
+  .then(dbDoc => dbDocToProseMirrorDoc(dbDoc, mySchema))
+  .then(doc => {
+    window.view = new EditorView(document.querySelector('#editor'), {
+      state: EditorState.create({
+        doc,
+        plugins: exampleSetup({schema: mySchema}),
+      }),
+    });
+  });
