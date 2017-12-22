67e78b9207485bcfd639de487affaa33e63159dc
Atul Varma <varmaa@gmail.com>
2017-12-22 09:59:17 -0500

    Pull functions/constants out of closure.

--- api/ombpdf/js/prosemirror-fun/convert-doc.js
+++ api/ombpdf/js/prosemirror-fun/convert-doc.js
@@ -2,75 +2,74 @@ import {Node} from "prosemirror-model";
 
 import schema from './policy-schema';
 
+function convertChild(node) {
+  if (node.node_type) {
+    if (node.node_type in NODE_TYPE_CONVERTERS) {
+      return NODE_TYPE_CONVERTERS[node.node_type](node);
+    }
+    console.warn(`Unknown node type: ${node.node_type}`);
+    return {
+      type: 'text',
+      text: node.node_type,
+      marks: [{type: 'unimplemented',
+               attrs: {data: JSON.stringify(node)}}],
+    };
+  }
+}
+
+function convertContent(node) {
+  if (node.content_type) {
+    if (node.content_type in CONTENT_TYPE_CONVERTERS) {
+      return CONTENT_TYPE_CONVERTERS[node.content_type](node);
+    }
+
+    console.warn(`Unknown content type: ${node.content_type}`);
+    return {
+      type: 'text',
+      text: node.content_type,
+      marks: [{type: 'unimplemented',
+               attrs: {data: JSON.stringify(node)}}],
+    };
+  }
+}
+
+function convertContentsAndChildren(node) {
+  const contents = node.content.map(convertContent);
+  const children = node.children.map(convertChild);
+  return contents.concat(children);
+};
+
+const NODE_TYPE_CONVERTERS = {
+  heading(node) {
+    return {
+      type: 'heading',
+      content: node.content.map(convertContent),
+    };
+  },
+  para(node) {
+    return {
+      type: 'paragraph',
+      content: convertContentsAndChildren(node),
+    };
+  },
+  sec(node) {
+    return {
+      type: 'section',
+      content: node.children.map(convertChild),
+    };
+  },
+};
+
+const CONTENT_TYPE_CONVERTERS = {
+  __text__(node) {
+    return {
+      type: 'text',
+      text: node.text.replace(/\s+/g, ' '),
+    };
+  }
+};
 
 export default function dbDocToProseMirrorDoc(root) {
-  const convertChild = node => {
-    if (node.node_type) {
-      if (node.node_type in node_type_converters) {
-        return node_type_converters[node.node_type](node);
-      }
-      console.warn(`Unknown node type: ${node.node_type}`);
-      return {
-        type: 'text',
-        text: node.node_type,
-        marks: [{type: 'unimplemented',
-                 attrs: {data: JSON.stringify(node)}}],
-      };
-    }
-  };
-
-  const convertContent = node => {
-    if (node.content_type) {
-      if (node.content_type in content_type_converters) {
-        return content_type_converters[node.content_type](node);
-      }
-
-      console.warn(`Unknown content type: ${node.content_type}`);
-      return {
-        type: 'text',
-        text: node.content_type,
-        marks: [{type: 'unimplemented',
-                 attrs: {data: JSON.stringify(node)}}],
-      };
-    }
-  };
-
-  const convertContentsAndChildren = node => {
-    const contents = node.content.map(convertContent);
-    const children = node.children.map(convertChild);
-    return contents.concat(children);
-  };
-
-  const node_type_converters = {
-    heading(node) {
-      return {
-        type: 'heading',
-        content: node.content.map(convertContent),
-      };
-    },
-    para(node) {
-      return {
-        type: 'paragraph',
-        content: convertContentsAndChildren(node),
-      };
-    },
-    sec(node) {
-      return {
-        type: 'section',
-        content: node.children.map(convertChild),
-      };
-    },
-  };
-
-  const content_type_converters = {
-    __text__(node) {
-      return {
-        type: 'text',
-        text: node.text.replace(/\s+/g, ' '),
-      };
-    }
-  };
-
   const doc = Node.fromJSON(schema, {
     type: 'doc',
     content: root.children
