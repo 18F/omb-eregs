79d6c31c19bdeca6eb00a927bfef066f99a5c3b8
CM Lubinski <christopher.lubinski@gsa.gov>
2017-12-29 16:10:21 -0500

    Display list item titles, if present.
    We've seen multiple policies where individual list items have an associated
    title/heading. This modifies our renderers to display that, if present.

--- ui/__tests__/components/node-renderers/list-item.test.js
+++ ui/__tests__/components/node-renderers/list-item.test.js
@@ -18,5 +18,16 @@ describe('<ListItem />', () => {
     const text = shallow(<ListItem docNode={docNode} />).text();
     expect(text).toMatch(/c\.!!!/);
   });
+  it('includes the title if present', () => {
+    const docNode = new DocumentNode({ title: 'Some title' });
+    const result = shallow(<ListItem docNode={docNode} />).find('.list-item-title');
+    expect(result).toHaveLength(1);
+    expect(result.text()).toBe('Some title');
+  });
+  it('does not include a title if not present', () => {
+    const docNode = new DocumentNode();
+    const result = shallow(<ListItem docNode={docNode} />).find('.list-item-title');
+    expect(result).toHaveLength(0);
+  });
 });
 
