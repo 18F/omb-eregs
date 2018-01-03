08b490a0ce23e900fd4198f6c53e758c89e95e60
CM Lubinski <christopher.lubinski@gsa.gov>
2018-01-03 14:01:33 -0500

    Pull out, refactor calc_left_edge function.

--- api/ombpdf/tests/test_document.py
+++ api/ombpdf/tests/test_document.py
@@ -1,3 +1,8 @@
+from unittest.mock import Mock
+
+from ombpdf import document
+
+
 def test_textline_repr_works(m_16_19_doc):
     assert repr(m_16_19_doc.pages[0][4]) == \
         '<OMBTextLine with text "August 1, 2016 ">'
@@ -17,3 +22,15 @@ def test_useful_lines_are_not_culled(m_11_29_doc):
     lastpage_text = '\n'.join([str(line) for line in m_11_29_doc.pages[-1]])
 
     assert 'opportunities to reduce duplication' in lastpage_text
+
+
+def test_calc_left_edge():
+    lines = [
+        Mock(left_edge=10),
+        Mock(left_edge=20),
+        Mock(left_edge=10),
+        Mock(left_edge=20),
+        Mock(left_edge=30),
+        Mock(left_edge=10),
+    ]
+    assert document.calc_left_edge(lines) == 10
