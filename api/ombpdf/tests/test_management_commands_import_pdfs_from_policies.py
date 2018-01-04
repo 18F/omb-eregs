5c8d20ef016af79bd3fb0ce3ec256a03e099cb11
CM Lubinski <christopher.lubinski@gsa.gov>
2018-01-04 13:17:43 -0500

    Add command to import PDFs based on policy data.
    Policies were initially loaded with a source URI (they might also have a
    replacement pdf). We can use that, along with our knowledge of the Obama
    whitehouse archive to import remaining pdfs.

---
+++ api/ombpdf/tests/test_management_commands_import_pdfs_from_policies.py
@@ -0,0 +1,98 @@
+from unittest.mock import Mock, call
+
+import pytest
+from django.core.files import File
+from model_mommy import mommy
+from requests.exceptions import ConnectionError
+
+from ombpdf.management.commands import import_pdfs_from_policies
+from reqs.models import Policy
+
+
+def test_best_url(monkeypatch):
+    monkeypatch.setattr(import_pdfs_from_policies, 'requests', Mock())
+    import_pdfs_from_policies.requests.head.side_effect = [
+        Mock(status_code=404),
+        Mock(status_code=500),
+        Mock(status_code=200),
+        Mock(status_code=200),
+    ]
+
+    result = import_pdfs_from_policies.best_url(['aaa', 'bbb', 'ccc', 'ddd'])
+    assert result == 'ccc'
+
+
+def test_best_url_exception(monkeypatch):
+    monkeypatch.setattr(import_pdfs_from_policies, 'requests', Mock())
+    import_pdfs_from_policies.requests.head.side_effect = ConnectionError()
+
+    result = import_pdfs_from_policies.best_url(['aaa', 'bbb'])
+    assert result is None
+
+
+def test_import_from_policy_obama_whitehouse(monkeypatch):
+    monkeypatch.setattr(import_pdfs_from_policies, 'best_url', Mock())
+    monkeypatch.setattr(import_pdfs_from_policies, 'parse_pdf', Mock())
+    policy = mommy.prepare(Policy, uri='https://www.whitehouse.gov/some/place')
+
+    import_pdfs_from_policies.import_from_policy(policy)
+    assert import_pdfs_from_policies.best_url.call_args == call([
+        'https://www.whitehouse.gov/some/place',
+        'https://obamawhitehouse.archives.gov/some/place',
+    ])
+
+
+def test_import_from_policy_attached_file(monkeypatch):
+    monkeypatch.setattr(import_pdfs_from_policies, 'best_url', Mock())
+    monkeypatch.setattr(import_pdfs_from_policies, 'parse_pdf', Mock())
+    policy = mommy.prepare(
+        Policy,
+        uri='https://example.com/',
+        document_source=File(b'stuff', name='a_file.pdf'),
+    )
+
+    import_pdfs_from_policies.import_from_policy(policy)
+    assert 'a_file.pdf' in policy.document_source.url
+    assert import_pdfs_from_policies.best_url.call_args == call([
+        policy.document_source.url,
+        'https://example.com/',
+    ])
+
+
+def test_import_from_policy_failures(monkeypatch):
+    monkeypatch.setattr(import_pdfs_from_policies, 'best_url', Mock())
+    monkeypatch.setattr(import_pdfs_from_policies, 'parse_pdf', Mock())
+    policy = mommy.prepare(Policy)
+
+    import_pdfs_from_policies.best_url.return_value = True
+    assert import_pdfs_from_policies.import_from_policy(policy)
+
+    import_pdfs_from_policies.best_url.return_value = False
+    assert not import_pdfs_from_policies.import_from_policy(policy)
+
+    import_pdfs_from_policies.best_url.return_value = True
+    import_pdfs_from_policies.parse_pdf.return_value = False
+    assert not import_pdfs_from_policies.import_from_policy(policy)
+
+
+@pytest.mark.django_db
+def test_import_pdfs_from_policies(monkeypatch):
+    monkeypatch.setattr(import_pdfs_from_policies, 'import_from_policy',
+                        Mock())
+    import_pdfs_from_policies.import_from_policy.side_effect = \
+        lambda p: p.title.startswith('no-exception')
+    p1 = mommy.make(Policy, title='no-exception-1',
+                    uri='http://example.com/some/thing.pdf')
+    mommy.make(Policy, title='no-exeception-but-not-a-pdf',
+               uri='http://example.com/not-a/pdf')
+    p3 = mommy.make(Policy, title='no-exception-2',
+                    uri='http://example.com/other/pdf.pdf')
+    p4 = mommy.make(Policy, title='exception-but-a-pdf',
+                    uri='http://example.com/an/exception.pdf')
+
+    successes, failures = import_pdfs_from_policies.import_pdfs_from_policies()
+
+    assert successes == {f'{p1.pk}: no-exception-1',
+                         f'{p3.pk}: no-exception-2'}
+    assert failures == {f'{p4.pk}: exception-but-a-pdf'}
+    # p2 doesn't show up as it's not a pdf
