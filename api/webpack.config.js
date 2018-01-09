b43b42b54e305985ec3d51c3b3835369b1e65736
CM Lubinski <christopher.lubinski@gsa.gov>
2018-01-09 15:14:13 -0500

    Configure webpack for admin styles override.
    While we can configure some of the Django admin interface, we'll also want a
    hook to override styles. This configures webpack to build an override from
    SCSS.
    
    Note that the ExtractText plugin is needed to generate a separate CSS file
    (rather than inlining with the JS).

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,35 +1,67 @@
-var webpack = require('webpack');
-var path = require('path');
+const path = require('path');
 
-module.exports = {
-  context: __dirname,
-  devtool: 'inline-source-map',
-  entry: {
-    'document.main': './document/js/main.ts',
-    'ombpdf.main': './ombpdf/js/main.ts',
-    'pdf.worker': 'pdfjs-dist/build/pdf.worker.entry'
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = [
+  {
+    name: 'editor-js',
+    context: __dirname,
+    devtool: 'inline-source-map',
+    entry: {
+      'document.main': './document/js/main.ts',
+      'ombpdf.main': './ombpdf/js/main.ts',
+      'pdf.worker': 'pdfjs-dist/build/pdf.worker.entry'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: 'ts-loader' },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    output: {
+      path: path.join(__dirname, 'webpack-static/bundles/js/'),
+      publicPath: 'static/bundles/js/',
+      filename: '[name].bundle.js',
+    },
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+  {
+    name: 'admin-styles',
+    context: __dirname,
+    devtool: 'source-map',
+    entry: path.join(__dirname, 'ereqs_admin', 'static', 'admin', 'override.scss'),
+    output: {
+      path: path.join(__dirname, 'webpack-static', 'bundles', 'css', 'admin'),
+      publicPath: 'static/bundles/css/admin/',
+      filename: 'override.css',
+    },
+    plugins: [new ExtractTextPlugin('override.css')],
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader',
+              options: { sourceMap: true } },
+            { loader: 'sass-loader',
+              options: { sourceMap: true } },
+          ],
+        }),
+      }],
+    },
   },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  output: {
-    path: path.join(__dirname, 'webpack-static/bundles/js/'),
-    publicPath: 'static/bundles/js/',
-    filename: '[name].bundle.js'
-  }
-};
+];
 
 if (process.env.USE_POLLING === 'true') {
-  module.exports.watchOptions = {
-    poll: true,
-  };
+  module.exports.forEach(exp => {
+    exp.watchOptions = { poll: true };
+  });
 }
