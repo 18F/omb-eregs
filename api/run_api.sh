2d92482cf5105bc7d00a14e9fbd4c723ba26b215
CM Lubinski <christopher.lubinski@gsa.gov>
2018-01-10 14:59:52 -0500

    Run collectstatic in CI rather than cloud.gov.
    Hypothetically, this reduces our restage time, but the true benefit is that we
    don't need to have all the source files available in production. Without that
    requirement, we can share the source files between the UI and admin pretty
    easily.

--- api/run_api.sh
+++ api/run_api.sh
@@ -4,6 +4,5 @@
 if [ -n "$DEBUG" ]; then
   ./manage.py runserver 0.0.0.0:"$PORT"
 else
-  ./manage.py collectstatic --noinput
   gunicorn omb_eregs.wsgi:application -b 0.0.0.0:"$PORT" --access-logfile -
 fi
