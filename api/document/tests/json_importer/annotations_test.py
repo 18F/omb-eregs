03b10ad1fbfdb08ccc7f593f7ceccc834941a8d4
Atul Varma <varmaa@gmail.com>
2018-01-16 07:26:27 -0500

    100% test coverage on json_importer.annotations_test.

--- api/document/tests/json_importer/annotations_test.py
+++ api/document/tests/json_importer/annotations_test.py
@@ -1,8 +1,26 @@
+from unittest.mock import Mock
+
+import pytest
+
 from document.json_importer.annotations import derive_annotations
 from document.json_importer.importer import convert_node
 from document.models import ExternalLink, FootnoteCitation
 
 from .. import factories as f
+
+
+def test_derive_annotations_works_on_children():
+    para = convert_node(f.para(content=[f.text('hi')], children=[
+        f.para(content=[f.external_link('http://one.org', [
+            f.text('blah')
+        ])])
+    ]))
+
+    annos = derive_annotations(para)
+
+    assert len(annos) == 1
+    links = annos[ExternalLink]
+    assert len(links) == 1
 
 
 def test_derive_annotations_works_with_nested_content():
@@ -60,3 +78,22 @@ def test_derive_annotations_works_with_footnote_citation():
     assert cit.start == 0
     assert cit.end == 1
     assert cit.footnote_node.text == 'Hi I am a footnote'
+
+
+def test_derive_annotations_raises_err_on_invalid_footnote_citation():
+    para = convert_node(f.para(
+        content=[f.footnote_citation([f.text('3')])],
+        children=[f.footnote(4, content=[f.text('Hi I am a footnote')])]
+    ))
+
+    with pytest.raises(ValueError,
+                       match="unable to find footnote for citation 3"):
+        derive_annotations(para)
+
+
+def test_derive_annotations_raises_err_on_invalid_annotation():
+    para = Mock(json_content=[{'content_type': 'blarg'}])
+
+    with pytest.raises(ValueError,
+                       match="no annotator found for blarg"):
+        derive_annotations(para)
