b657fe81bff5347518fcbfba8763c9af6a3155d7
CM Lubinski <christopher.lubinski@gsa.gov>
2018-01-17 09:39:05 -0500

    Move editor location, add auth.
    By adding the document id to the editor's path we can ensure that the
    requested policy exists and 404 if not. This also adds an authentication
    check. It doesn't try to evaluate public vs non-public status.

--- api/document/urls.py
+++ api/document/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
-from document.views import TreeView, editor
+from document.views import TreeView
 
 urlpatterns = [
-    url(r'^editor$', editor),
     url(r'^(?P<policy_id>[^/]+)(/(?P<identifier>[a-zA-Z0-9_-]+))?',
         TreeView.as_view()),
 ]
