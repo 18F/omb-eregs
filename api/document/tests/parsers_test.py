811ac54efbc7c0f87a2f10e3f6de0f9234d2d61b
Atul Varma <varmaa@gmail.com>
2018-01-17 11:27:16 -0500

    Add billion laughs test. Also convert CRLFs.

--- api/document/tests/parsers_test.py
+++ api/document/tests/parsers_test.py
@@ -11,6 +11,25 @@ from document.tree import PrimitiveDict
 from . import factories as f
 
 
+# https://en.wikipedia.org/wiki/Billion_laughs
+BILLION_LAUGHS_XML = """\
+<?xml version="1.0"?>
+<!DOCTYPE lolz [
+ <!ENTITY lol "lol">
+ <!ELEMENT lolz (#PCDATA)>
+ <!ENTITY lol1 "&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;">
+ <!ENTITY lol2 "&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;">
+ <!ENTITY lol3 "&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;">
+ <!ENTITY lol4 "&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;">
+ <!ENTITY lol5 "&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;">
+ <!ENTITY lol6 "&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;">
+ <!ENTITY lol7 "&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;">
+ <!ENTITY lol8 "&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;">
+ <!ENTITY lol9 "&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;">
+]>
+<lolz>&lol9;</lolz>"""
+
+
 def convert_xml_node(text: str) -> PrimitiveDict:
     return parsers.convert_node(etree.fromstring(text))
 
@@ -28,6 +47,14 @@ def test_parser_works():
         "content": [],
         "children": [],
     }
+
+
+def test_parser_is_resilient_to_billion_laughs():
+    parser = parsers.AkomaNtosoParser()
+    stream = BytesIO(BILLION_LAUGHS_XML.encode('utf-8'))
+    with pytest.raises(etree.XMLSyntaxError,
+                       match="Detected an entity reference loop"):
+        parser.parse(stream)
 
 
 def test_num_is_converted():
