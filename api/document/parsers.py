52801f54a1670bc1e82a2b1e1eb6086bf66f5663
Atul Varma <varmaa@gmail.com>
2018-01-17 09:35:51 -0500

    Add Akoma Ntoso parser.

---
+++ api/document/parsers.py
@@ -0,0 +1,61 @@
+from typing import List
+
+from lxml import etree
+from rest_framework.exceptions import ParseError
+from rest_framework.parsers import BaseParser
+
+from document.tree import PrimitiveDict
+
+
+def text(text: str) -> PrimitiveDict:
+    return {
+        'content_type': '__text__',
+        'text': text
+    }
+
+
+def convert_content(xml_el: etree.Element) -> List[PrimitiveDict]:
+    data = []
+    if xml_el.text:
+        data.append(text(xml_el.text))
+    for child in xml_el:
+        data.append({
+            **child.attrib,
+            'content_type': child.tag,
+            'inlines': convert_content(child)
+        })
+        if child.tail:
+            data.append(text(child.tail))
+    return data
+
+
+def ensure_at_most_one_child_has_tag(xml_el: etree.Element, tag: str):
+    if len([child for child in xml_el if child.tag == tag]) > 1:
+        raise ParseError(f'<{xml_el.tag}> contains multiple '
+                         f'<{tag}> elements')
+
+
+def convert_node(xml_el: etree.Element) -> PrimitiveDict:
+    data = {
+        'node_type': xml_el.tag,
+        'children': [],
+        'content': [],
+    }
+    ensure_at_most_one_child_has_tag(xml_el, 'content')
+    ensure_at_most_one_child_has_tag(xml_el, 'num')
+    for child in xml_el:
+        if child.tag == 'content':
+            data['content'] = convert_content(child)
+        elif child.tag == 'num':
+            data['marker'] = child.text
+        else:
+            data['children'].append(convert_node(child))
+    return {**xml_el.attrib, **data}
+
+
+class AkomaNtosoParser(BaseParser):
+    media_type = 'application/akn+xml'
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        root = etree.fromstring(stream.read().decode('utf-8'))
+        return convert_node(root)
---
+++ api/document/tests/parsers_test.py
@@ -0,0 +1,106 @@
+from io import BytesIO
+from typing import List
+
+import pytest
+from lxml import etree
+from rest_framework.exceptions import ParseError
+
+from document import parsers
+from document.tree import PrimitiveDict
+
+from . import factories as f
+
+
+def convert_xml_node(text: str) -> PrimitiveDict:
+    return parsers.convert_node(etree.fromstring(text))
+
+
+def convert_content_node(text) -> List[PrimitiveDict]:
+    return convert_xml_node(f'<a><content>{text}</content></a>')['content']
+
+
+def test_parser_works():
+    parser = parsers.AkomaNtosoParser()
+    stream = BytesIO(b'<policy></policy>')
+    data = parser.parse(stream)
+    assert data == {
+        "node_type": "policy",
+        "content": [],
+        "children": [],
+    }
+
+
+def test_num_is_converted():
+    assert 'marker' not in convert_xml_node('<b/>')
+    assert convert_xml_node('<b><num>3</num></b>')['marker'] == '3'
+
+
+def test_error_raised_on_multiple_num_elements():
+    with pytest.raises(ParseError,
+                       match="<a> contains multiple <num> elements"):
+        convert_xml_node('<a><num/><num/></a>')
+
+
+def test_node_attribs_are_converted():
+    assert 'title' not in convert_xml_node('<blah/>')
+    assert convert_xml_node('<blah title="foo"/>')['title'] == 'foo'
+
+
+def test_content_attribs_are_converted():
+    assert 'title' not in convert_content_node('<b/>')[0]
+    assert convert_content_node('<b title="foo"/>')[0]['title'] == 'foo'
+
+
+def test_xml_attribs_do_not_overwrite_node_data_keys():
+    assert convert_xml_node('<a node_type="b"/>')['node_type'] == 'a'
+
+
+def test_xml_attribs_do_not_overwrite_content_data_keys():
+    assert convert_content_node(
+        '<b content_type="c"/>'
+    )[0]['content_type'] == 'b'
+
+
+def test_content_always_exists():
+    assert convert_xml_node('<blah/>')['content'] == []
+    assert convert_xml_node('<blah><content/></blah>')['content'] == []
+
+
+def test_bare_content_text_is_converted():
+    assert convert_xml_node('<a><content>hi</content></a>')['content'] == [
+        f.text('hi')
+    ]
+
+
+def test_nested_content_text_is_converted():
+    assert convert_content_node(
+        'hi <a>there <b>buddy</b> ole</a> pal'
+    ) == [
+        f.text('hi '),
+        {
+            'content_type': 'a',
+            'inlines': [
+                f.text('there '),
+                {
+                    'content_type': 'b',
+                    'inlines': [f.text('buddy')],
+                },
+                f.text(' ole'),
+            ],
+        },
+        f.text(' pal'),
+    ]
+
+
+def test_error_raised_on_multiple_content_elements():
+    with pytest.raises(ParseError,
+                       match="<a> contains multiple <content> elements"):
+        convert_xml_node('<a><content/><content/></a>')
+
+
+def test_children_are_converted():
+    assert convert_xml_node('<a><b/></a>')['children'] == [{
+        "node_type": "b",
+        "content": [],
+        "children": [],
+    }]
