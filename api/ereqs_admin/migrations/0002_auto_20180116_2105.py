6b824e354e729446e3c810597d5c1c2894a84b68
CM Lubinski <christopher.lubinski@gsa.gov>
2018-01-18 09:24:34 -0500

    Fixups and tweaks for #866
    * Define the missing "Theme" variable
    * Avoid creating an unused variable
    * Filter by `name` rather than `pk`, which might vary
    * Update theme name as we're no longer using USWDS colors
    * Add newline to the colors file to satisfy sass lint rules

--- api/ereqs_admin/migrations/0002_auto_20180116_2105.py
+++ api/ereqs_admin/migrations/0002_auto_20180116_2105.py
@@ -6,7 +6,8 @@ from django.db import migrations
 
 
 def forward(apps, schema_editor):
-    uswds = Theme.objects.filter(pk=1).update(
+    Theme = apps.get_model('admin_interface', 'Theme')
+    Theme.objects.filter(name='USWDS').update(
         css_header_background_color='#000000',
         css_header_link_hover_color='#edfcff',
         css_module_background_color='#222222',
@@ -16,11 +17,13 @@ def forward(apps, schema_editor):
         css_generic_link_color='#112e51',
         css_generic_link_hover_color='#1067a6',
         css_save_button_background_color='#1067a6',
-        css_save_button_background_hover_color='#112e51'
+        css_save_button_background_hover_color='#112e51',
+        name='OMB',
     )
 
 def backwards(apps, schema_editor):
-    uswds = Theme.objects.filter(pk=1).update(
+    Theme = apps.get_model('admin_interface', 'Theme')
+    Theme.objects.filter(name='OMB').update(
         css_header_background_color='#112E51',
         css_header_link_hover_color='#E1F3F8',
         css_module_background_color='#205493',
@@ -30,7 +33,8 @@ def backwards(apps, schema_editor):
         css_generic_link_color='#205493',
         css_generic_link_hover_color='#0071BC',
         css_save_button_background_color='#205493',
-        css_save_button_background_hover_color='#112E51'
+        css_save_button_background_hover_color='#112E51',
+        name='USWDS',
     )
 
 class Migration(migrations.Migration):
