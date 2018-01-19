cb97235ca512fd206a150674bd8f0e704b4186a1
Atul Varma <varmaa@gmail.com>
2018-01-19 16:02:53 -0500

    lint akn files and fix lint errors.

--- api/document/js/akn/main.ts
+++ api/document/js/akn/main.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import * as CodeMirror from "codemirror";
-import "codemirror/mode/xml/xml.js";
-import "codemirror/addon/search/search.js";
-import "codemirror/addon/search/searchcursor.js";
-import "codemirror/addon/dialog/dialog.js";
+import * as CodeMirror from 'codemirror';
+import 'codemirror/mode/xml/xml.js';
+import 'codemirror/addon/search/search.js';
+import 'codemirror/addon/search/searchcursor.js';
+import 'codemirror/addon/dialog/dialog.js';
 
-import { getEl } from "../util";
+import { getEl } from '../util';
 
 
 // We need to load our CSS via require() rather than import;
@@ -37,52 +37,52 @@ function saveDoc(data: string, path?: string) {
   const pathParts = (path || window.location.href).split('/');
   const policyId = pathParts[pathParts.length - 2];
   return axios({
+    data,
     method: 'put',
     url: `/document/${policyId}`,
-    data,
     headers: {
       'Content-Type': 'application/akn+xml',
       'X-CSRFToken': getEl('[name=csrfmiddlewaretoken]')
         .getAttribute('value'),
     },
-  })
+  });
 }
 
 function setStatusError(e: Error) {
   let errMsg = `An error occurred: ${e}`;
-  let data = e['response'] && e['response']['data'];
+  const data = e['response'] && e['response']['data'];
   if (data) {
     errMsg += '\n' + JSON.stringify(data, null, 2);
   }
-  setStatus(errMsg, "error");
+  setStatus(errMsg, 'error');
 }
 
-function setStatus(msg: string, className: "error"|"" = "") {
-  let status = getEl("#status");
+function setStatus(msg: string, className: 'error'|'' = '') {
+  const status = getEl('#status');
   status.textContent = msg;
   status.className = className;
 }
 
 function createEditor(value: string) {
   const cm = CodeMirror(getEl(EDITOR_SEL), {
+    value,
     lineNumbers: true,
     mode: 'xml',
     theme: 'eclipse',
-    value,
   });
 
-  CodeMirror['commands'].save = cm => {
-    setStatus("Saving...");
-    saveDoc(cm.getValue()).then(_ => {
+  CodeMirror['commands'].save = (cm) => {
+    setStatus('Saving...');
+    saveDoc(cm.getValue()).then((_) => {
       setStatus(`Document saved at ${new Date()}.`);
     }).catch(setStatusError);
   };
 }
 
 window.addEventListener('load', () => {
-  setStatus("Loading document...");
-  fetchDoc().then(value => {
-    setStatus("Document loaded.");
+  setStatus('Loading document...');
+  fetchDoc().then((value) => {
+    setStatus('Document loaded.');
     createEditor(value);
   }).catch(setStatusError);
 });
