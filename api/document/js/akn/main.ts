5299970aa7f612eaa1125d74f3dd175331778f1a
CM Lubinski <christopher.lubinski@gsa.gov>
2018-01-23 17:10:07 -0500

    Pull out API interactions from akn editor.
    We'll be able to re-use these in the prosemirror editor. Also adds tests.

--- api/document/js/akn/main.ts
+++ api/document/js/akn/main.ts
@@ -5,49 +5,14 @@ import 'codemirror/addon/search/search.js';
 import 'codemirror/addon/search/searchcursor.js';
 import 'codemirror/addon/dialog/dialog.js';
 
+import Api from '../Api';
 import { getEl, getElAttr } from '../util';
-
 
 const EDITOR_SEL = '#editor';
 const DOC_URL_ATTR = 'data-document-url';
 const AKN_CONTENT_TYPE = 'application/akn+xml';
 
-function fetchDoc() {
-  return axios({
-    method: 'GET',
-    url: getElAttr(EDITOR_SEL, DOC_URL_ATTR),
-    headers: { Accept: AKN_CONTENT_TYPE },
-  }).then(response => response.data);
-}
-
-function saveDoc(data: string) {
-  return axios({
-    data,
-    method: 'put',
-    url: getElAttr(EDITOR_SEL, DOC_URL_ATTR),
-    headers: {
-      'Content-Type': AKN_CONTENT_TYPE,
-      'X-CSRFToken': getElAttr('[name=csrfmiddlewaretoken]', 'value'),
-    },
-  });
-}
-
-function setStatusError(e: Error) {
-  let errMsg = `An error occurred: ${e}`;
-  const data = e['response'] && e['response']['data'];
-  if (data) {
-    errMsg += '\n' + JSON.stringify(data, null, 2);
-  }
-  setStatus(errMsg, 'editor-status-error');
-}
-
-function setStatus(msg: string, className: 'editor-status-error'|'' = '') {
-  const status = getEl('#status');
-  status.textContent = msg;
-  status.className = className;
-}
-
-function createEditor(value: string) {
+function createEditor(value: string, api: Api) {
   const cm = CodeMirror(getEl(EDITOR_SEL), {
     value,
     lineNumbers: true,
@@ -55,18 +20,15 @@ function createEditor(value: string) {
     theme: 'eclipse',
   });
 
-  CodeMirror['commands'].save = (cm) => {
-    setStatus('Saving...');
-    saveDoc(cm.getValue()).then((_) => {
-      setStatus(`Document saved at ${new Date()}.`);
-    }).catch(setStatusError);
-  };
+  CodeMirror['commands'].save = cm => api.write(cm.getValue());
 }
 
 window.addEventListener('load', () => {
-  setStatus('Loading document...');
-  fetchDoc().then((value) => {
-    setStatus('Document loaded.');
-    createEditor(value);
-  }).catch(setStatusError);
+  const api = new Api(
+    getElAttr(EDITOR_SEL, DOC_URL_ATTR),
+    AKN_CONTENT_TYPE,
+    getElAttr('[name=csrfmiddlewaretoken]', 'value'),
+  );
+
+  api.fetch().then(data => createEditor(data, api));
 });
