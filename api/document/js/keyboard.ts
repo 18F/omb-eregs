56f0072b90724542d98c208009e8a22f66a63242
CM Lubinski <christopher.lubinski@gsa.gov>
2018-02-08 11:18:17 -0500

    Remove joinBackwards/forwards from backspace/del.
    These commands would connect text elements across elements, which might not
    always make sense. For example, when a user presses "delete" at the end of a
    paragraph with a footnote, do we expect the footnote text to be merged with
    the paragraph? If we backspace at the front of a paragraph after a heading, do
    we expect the paragraph text to be placed in the heading?
    
    Rather than figuring all of these scenarios out, seems like we should punt for
    now.

--- api/document/js/keyboard.ts
+++ api/document/js/keyboard.ts
@@ -1,4 +1,4 @@
-import { baseKeymap } from 'prosemirror-commands';
+import { baseKeymap, chainCommands, deleteSelection, selectNodeBackward, selectNodeForward } from 'prosemirror-commands';
 import { undo, redo } from 'prosemirror-history';
 import { keymap } from 'prosemirror-keymap';
 
@@ -6,11 +6,28 @@ import Api from './Api';
 import { makeSave } from './commands';
 import schema from './schema';
 
+// Removing joinBackwards/forwards from actions taken when deleting as they
+// need to be tuned a bit better to our use case.
+const backspace = chainCommands(deleteSelection, selectNodeBackward);
+const del = chainCommands(deleteSelection, selectNodeForward);
+
 export default function menu(api: Api) {
   return keymap({
     ...baseKeymap,
+    'Backspace': backspace,
+    'Mod-Backspace': backspace,
+    'Delete': del,
+    'Mod-Delete': del,
     'Mod-z': undo,
     'Shift-Mod-z': redo,
     'Mod-s': makeSave(api),
+    // Macs have additional keyboard combinations for deletion; we set them
+    // for all OSes as a convenience
+    'Ctrl-h': backspace,
+    'ALt-Backspace': backspace,
+    'Ctrl-d': del,
+    'Ctrl-Alt-Backspace': del,
+    'Alt-Delete': del,
+    'Alt-d': del,
   });
 }
