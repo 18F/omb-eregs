9bb725c41469f7ee6ad2fea42e775b3bfac8193d
CM Lubinski <christopher.lubinski@gsa.gov>
2018-02-15 19:53:46 -0500

    Add fns for collapsing/renumbering adjacent lists.
    When we outdent, we'll want to combine any previously-nested-sublist(s) with
    now-adjacent sublists. We'll also need to renumber these adjacent lists to use
    the type of marker the previously-indented listitem had previously.

--- api/document/js/list-utils.ts
+++ api/document/js/list-utils.ts
@@ -130,6 +130,64 @@ function renumberSublist(
   return result;
 }
 
+// Given a position arbitrarily deep in the document hierarchy, find a
+// following block node and return a position within it
+function adjacentBlock(resolved: ResolvedPos): ResolvedPos | void {
+  const blockDepth = walkUpUntil(resolved, n => n.type.spec.group === 'block');
+  const posAfterBlock = resolved.after(blockDepth);
+  const parentDepth = blockDepth - 1;
+  if (parentDepth >= 0) {
+    const parent = resolved.node(parentDepth);
+    const blockIdxInParent = resolved.index(parentDepth);
+    if (parent.childCount > blockIdxInParent + 1) {
+      return resolved.doc.resolve(posAfterBlock + 1); // enter the new block
+    }
+  }
+}
+
+// Given a pos, find any adjacent, sequential lists. If the exist, collapse
+// them, using the marker template from the first
+export function collapseAdjacentLists(transaction: Transaction, pos: number): Transaction {
+  let result = transaction;
+  const resolved = result.doc.resolve(pos);
+  const neighbor1 = adjacentBlock(resolved);
+  const neighbor2 = adjacentBlock(neighbor1 || resolved);
+  if (neighbor1 && neighbor1.parent.type === schema.nodes.list
+      && neighbor2 && neighbor2.parent.type === schema.nodes.list) {
+    const list2 = neighbor2.parent;
+    result = result.delete(
+      neighbor2.before(neighbor2.depth),
+      neighbor2.after(neighbor2.depth),
+    );
+    result = result.insert(neighbor1.end(neighbor1.depth), list2.content);
+    result = collapseAdjacentLists(result, pos);  // repeat
+  }
+  return result;
+}
+
+// If there's an adjacent list, renumber it based on the provided marker
+// template.
+export function renumberAdjacentList(
+  transaction: Transaction,
+  markerTemplate: string,
+  pos: number,
+): Transaction {
+  let result = transaction;
+  const resolved = transaction.doc.resolve(pos);
+  const neighbor = adjacentBlock(resolved);
+  if (neighbor && neighbor.parent.type === schema.nodes.list) {
+    const startOfList = neighbor.start(neighbor.depth);
+    result = result.replaceWith(
+      neighbor.before(neighbor.depth),
+      neighbor.after(neighbor.depth),
+      factory.list(markerTemplate, neighbor.parent.content),
+    );
+    result = renumberList(result, startOfList);
+    result = renumberSublists(result, startOfList);
+  }
+  return result;
+}
+
 // Helpful for testing. Assumes the first parameter is a list node
 export function collectMarkers(list: Node): string[] {
   const result: string[] = [];
