b63591fbc8033f1caa7dd637485853d953503ec3
CM Lubinski <christopher.lubinski@gsa.gov>
2018-03-01 18:34:39 -0500

    Add two initial document migrations.
    The PDF parser will sometimes generate documents which aren't valid in our
    ProseMirror schema. This adds a command to migrate those documents to a format
    which _is_ valid.
    
    Ideally, this would include modifications to the PDF parser to prevent these
    documents from being created, but we're pressed for time.
    
    Also, we're able to cut some corners by ignoring potential changes to inline
    annotations. As we're only modifying existing docnodes (or adding new ones),
    we'll never break an FK. That won't be true of future doc migrations.

---
+++ api/ombpdf/management/commands/migrate_documents.py
@@ -0,0 +1,71 @@
+import logging
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from tqdm import tqdm
+
+from document.models import DocNode
+from document.tree import DocCursor
+
+logger = logging.getLogger(__name__)
+
+
+def ensure_listitem_in_list(doc: DocCursor) -> DocCursor:
+    """We expect all listitems to have a parent list. However, the parser has
+    emitted lists followed by bare listitems. We'll place the listitem in the
+    list that imemdiately precedes it."""
+    for li in doc.filter(lambda n: n.node_type == 'listitem'):
+        parent = li.parent()
+        prev_sibling = li.left_sibling()
+
+        if not parent:
+            logger.warning('Root of %s is an li.',
+                           doc.policy.title_with_number)
+            continue
+        if parent.node_type == 'list':  # all is well
+            continue
+
+        if prev_sibling and prev_sibling.node_type == 'list':
+            li.append_to(prev_sibling)
+            return ensure_listitem_in_list(doc)
+        # else: create new list to wrap this one
+        logger.warning('Could not fix li in %s', doc.policy.title_with_number)
+
+    return doc  # no changes needed
+
+
+def ensure_section_has_heading(doc: DocCursor) -> DocCursor:
+    """We expect all sections to have a heading. Fill the missing data with
+    placeholder text."""
+    # materialize so we don't need to worry about a modified iterator
+    secs = list(doc.filter(lambda n: n.node_type == 'sec'))
+    for sec in secs:
+        children = list(sec.children())
+        if not children or children[0].node_type != 'heading':
+            sec.add_child('heading', insert_pos=0, text='--Missing Heading--',
+                          policy_id=doc.policy_id)
+    return doc
+
+
+transforms = [
+    ensure_listitem_in_list,
+    ensure_section_has_heading,
+]
+
+
+class Command(BaseCommand):
+    help = ( # noqa (overriding a builtin)
+        "Run through (idempotent) document migrations to mass-fixup docs.")
+
+    def handle(self, *args, **kwargs):
+        roots = DocNode.objects.filter(depth=0)
+        with tqdm(total=roots.count()) as pbar:
+            for root_docnode in roots:
+                with transaction.atomic():
+                    doc = DocCursor.load_from_model(root_docnode)
+                    for transform in transforms:
+                        doc = transform(doc)
+                    doc.nested_set_renumber(bulk_create=False)
+                    for node in doc.walk():
+                        node.save()
+                pbar.update(1)
