b292b7b5695444aeba0cb8c3a8d30ca74b197baa
CM Lubinski <christopher.lubinski@gsa.gov>
2018-03-19 10:00:36 -0400

    Remove link to requirements.
    We don't want to emphasize requirements anymore; this (very minimal)
    resolution treats the requirement data as a percentage of the document. This
    could be misleading, but is better than the current, requirement-centric
    model.

--- ui/components/policies/policy-view.js
+++ ui/components/policies/policy-view.js
@@ -4,13 +4,8 @@ import React from 'react';
 import Policy from '../../util/policy';
 import Link from '../link';
 
-export default function PolicyView({ policy, topicsIds }) {
-  const linkParams = {
-    policy__id__in: policy.id,
-    topics__id__in: topicsIds,
-  };
-  const relevantReqCount = policy.relevantReqs >= 100 ? '99+' : policy.relevantReqs;
-  const countClass = relevantReqCount === '99+' ? 'ninety-nine-plus' : '';
+export default function PolicyView({ policy }) {
+  const percentMatch = (100 * (policy.relevantReqs / policy.totalReqs)).toFixed();
   let policyTitle = policy.titleWithNumber;
 
   if (policy.hasPublishedDocument) {
@@ -27,18 +22,7 @@ export default function PolicyView({ policy, topicsIds }) {
         <h2 className="mt0 mb3">{policyTitle}</h2>
         <div className="clearfix">
           <div className="requirements-links mb1 sm-col sm-col-12 md-col-8">
-            <div className="circle-bg border gray-border center p1">
-              <Link
-                route="requirements"
-                params={linkParams}
-                aria-label="Relevant requirements"
-                className={countClass}
-              >
-                {relevantReqCount}
-              </Link>
-            </div> of&nbsp;
-            {policy.totalReqs} requirements
-             match your search
+            {percentMatch}% match
           </div>
           <div className="sm-col sm-col-12 md-col-4 right-align">
             <Link href={policy.originalUrl}>
